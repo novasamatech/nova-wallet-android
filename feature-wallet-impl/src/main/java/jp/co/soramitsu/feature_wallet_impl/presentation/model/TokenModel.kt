8bf7f6e7b58f1808d6a7ec35a63b8b5abf4d14f6
Valentun <sergeev@soramitsu.co.jp>
2021-10-14 10:48:42 +0300

    Show zeros if fiat representation not exists

--- feature-wallet-impl/src/main/java/jp/co/soramitsu/feature_wallet_impl/presentation/model/TokenModel.kt
+++ feature-wallet-impl/src/main/java/jp/co/soramitsu/feature_wallet_impl/presentation/model/TokenModel.kt
@@ -1,24 +1,16 @@
 package jp.co.soramitsu.feature_wallet_impl.presentation.model
 
-import jp.co.soramitsu.common.utils.isNonNegative
+import androidx.annotation.ColorRes
 import jp.co.soramitsu.core.model.Node
-import jp.co.soramitsu.feature_wallet_impl.R
 import jp.co.soramitsu.runtime.multiNetwork.chain.model.Chain
 import java.math.BigDecimal
 
 class TokenModel(
     val configuration: Chain.Asset,
-    val dollarRate: BigDecimal?,
-    val recentRateChange: BigDecimal?
-) {
-    val rateChangeColorRes = determineChangeColor()
-
-    private fun determineChangeColor(): Int {
-        if (recentRateChange == null) return R.color.gray2
-
-        return if (recentRateChange.isNonNegative) R.color.green else R.color.red
-    }
-}
+    val dollarRate: BigDecimal,
+    val recentRateChange: BigDecimal,
+    @ColorRes val rateChangeColorRes: Int
+)
 
 // TODO - wallet compatibility
 val Chain.Asset.networkType
