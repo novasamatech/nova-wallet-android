97152e983b81359bb03c747cfa327247fdde257c
Valentun <sergeev@soramitsu.co.jp>
2021-10-21 13:30:55 +0300

    Add retries for chain fetching

--- runtime/src/main/java/io/novafoundation/nova/runtime/multiNetwork/chain/ChainSyncService.kt
+++ runtime/src/main/java/io/novafoundation/nova/runtime/multiNetwork/chain/ChainSyncService.kt
@@ -1,5 +1,6 @@
 package io.novafoundation.nova.runtime.multiNetwork.chain
 
+import io.novafoundation.nova.common.utils.retryUntilDone
 import io.novafoundation.nova.core_db.dao.ChainDao
 import io.novafoundation.nova.core_db.model.chain.JoinedChainInfo
 import io.novafoundation.nova.runtime.multiNetwork.chain.model.Chain
@@ -15,7 +16,8 @@ class ChainSyncService(
     suspend fun syncUp() = withContext(Dispatchers.Default) {
         val localChainsJoinedInfo = dao.getJoinChainInfo()
 
-        val remoteChains = chainFetcher.getChains().map(::mapChainRemoteToChain)
+        val remoteChains = retryUntilDone { chainFetcher.getChains() }.map(::mapChainRemoteToChain)
+
         val localChains = localChainsJoinedInfo.map(::mapChainLocalToChain)
 
         val remoteMapping = remoteChains.associateBy(Chain::id)
