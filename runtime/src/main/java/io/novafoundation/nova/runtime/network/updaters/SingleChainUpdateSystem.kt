6ffa96a3fabfe9df4d2a403611b8c758377155cd
Valentun <sergeev@soramitsu.co.jp>
2021-11-05 13:25:58 +0300

    Catch updater errors

--- runtime/src/main/java/io/novafoundation/nova/runtime/network/updaters/SingleChainUpdateSystem.kt
+++ runtime/src/main/java/io/novafoundation/nova/runtime/network/updaters/SingleChainUpdateSystem.kt
@@ -1,17 +1,20 @@
 package io.novafoundation.nova.runtime.network.updaters
 
+import android.util.Log
 import io.novafoundation.nova.common.data.network.StorageSubscriptionBuilder
+import io.novafoundation.nova.common.utils.LOG_TAG
 import io.novafoundation.nova.common.utils.hasModule
 import io.novafoundation.nova.core.updater.UpdateSystem
 import io.novafoundation.nova.core.updater.Updater
-import jp.co.soramitsu.fearless_utils.wsrpc.request.runtime.storage.subscribeUsing
 import io.novafoundation.nova.runtime.multiNetwork.ChainRegistry
 import io.novafoundation.nova.runtime.multiNetwork.getRuntime
 import io.novafoundation.nova.runtime.multiNetwork.getSocket
 import io.novafoundation.nova.runtime.state.SingleAssetSharedState
 import io.novafoundation.nova.runtime.state.selectedChainFlow
+import jp.co.soramitsu.fearless_utils.wsrpc.request.runtime.storage.subscribeUsing
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOn
@@ -28,13 +31,20 @@ class SingleChainUpdateSystem(
         val socket = chainRegistry.getSocket(chain.id)
         val runtimeMetadata = chainRegistry.getRuntime(chain.id).metadata
 
+        val logTag = this@SingleChainUpdateSystem.LOG_TAG
+        val selfName = this@SingleChainUpdateSystem::class.java.simpleName
+
         val scopeFlows = updaters.groupBy(Updater::scope).map { (scope, scopeUpdaters) ->
             scope.invalidationFlow().flatMapLatest {
                 val subscriptionBuilder = StorageSubscriptionBuilder.create(socket)
 
                 val updatersFlow = scopeUpdaters
                     .filter { it.requiredModules.all(runtimeMetadata::hasModule) }
-                    .map { it.listenForUpdates(subscriptionBuilder).flowOn(Dispatchers.Default) }
+                    .map { updater ->
+                        updater.listenForUpdates(subscriptionBuilder)
+                            .catch { Log.e(logTag, "Failed to start $selfName for ${chain.name}: ${it.message}") }
+                            .flowOn(Dispatchers.Default)
+                    }
 
                 if (updatersFlow.isNotEmpty()) {
                     val cancellable = socket.subscribeUsing(subscriptionBuilder.build())
