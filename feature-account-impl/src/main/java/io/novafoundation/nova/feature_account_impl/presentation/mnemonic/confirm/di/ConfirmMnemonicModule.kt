1ec5b3b78d9d31668dc3ce75e781f1da2b57ed7e
valentunn <70131744+valentunn@users.noreply.github.com>
2021-12-01 16:41:39 +0300

    Redesign/confirm mnemonic (#81)
    * Redesign confirm mnemonic
    
    * Code style

--- feature-account-impl/src/main/java/io/novafoundation/nova/feature_account_impl/presentation/mnemonic/confirm/di/ConfirmMnemonicModule.kt
+++ feature-account-impl/src/main/java/io/novafoundation/nova/feature_account_impl/presentation/mnemonic/confirm/di/ConfirmMnemonicModule.kt
@@ -6,18 +6,27 @@ import androidx.lifecycle.ViewModelProvider
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.IntoMap
+import io.novafoundation.nova.common.di.scope.ScreenScope
 import io.novafoundation.nova.common.di.viewmodel.ViewModelKey
 import io.novafoundation.nova.common.di.viewmodel.ViewModelModule
 import io.novafoundation.nova.common.resources.ResourceManager
 import io.novafoundation.nova.common.vibration.DeviceVibrator
 import io.novafoundation.nova.feature_account_api.domain.interfaces.AccountInteractor
+import io.novafoundation.nova.feature_account_impl.BuildConfig
 import io.novafoundation.nova.feature_account_impl.domain.account.add.AddAccountInteractor
 import io.novafoundation.nova.feature_account_impl.presentation.AccountRouter
+import io.novafoundation.nova.feature_account_impl.presentation.mnemonic.confirm.ConfirmMnemonicConfig
 import io.novafoundation.nova.feature_account_impl.presentation.mnemonic.confirm.ConfirmMnemonicPayload
 import io.novafoundation.nova.feature_account_impl.presentation.mnemonic.confirm.ConfirmMnemonicViewModel
 
 @Module(includes = [ViewModelModule::class])
 class ConfirmMnemonicModule {
+
+    @Provides
+    @ScreenScope
+    fun provideConfig() = ConfirmMnemonicConfig(
+        allowShowingSkip = BuildConfig.DEBUG
+    )
 
     @Provides
     @IntoMap
@@ -28,9 +37,10 @@ class ConfirmMnemonicModule {
         router: AccountRouter,
         deviceVibrator: DeviceVibrator,
         resourceManager: ResourceManager,
+        config: ConfirmMnemonicConfig,
         payload: ConfirmMnemonicPayload
     ): ViewModel {
-        return ConfirmMnemonicViewModel(interactor, addAccountInteractor, router, deviceVibrator, resourceManager, payload)
+        return ConfirmMnemonicViewModel(interactor, addAccountInteractor, router, deviceVibrator, resourceManager, config, payload)
     }
 
     @Provides
