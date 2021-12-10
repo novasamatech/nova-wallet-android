46212e803addd1b6658855f79a1afe35fd7a7366
valentun <sergeev@soramitsu.co.jp>
2021-12-10 17:17:00 +0400

    Code style

--- app/src/main/java/io/novafoundation/nova/app/di/app/navigation/DAppNavigationModule.kt
+++ app/src/main/java/io/novafoundation/nova/app/di/app/navigation/DAppNavigationModule.kt
@@ -3,12 +3,8 @@ package io.novafoundation.nova.app.di.app.navigation
 import dagger.Module
 import dagger.Provides
 import io.novafoundation.nova.app.root.navigation.NavigationHolder
-import io.novafoundation.nova.app.root.navigation.Navigator
-import io.novafoundation.nova.app.root.navigation.account.AdvancedEncryptionCommunicatorImpl
 import io.novafoundation.nova.app.root.navigation.dApp.DAppNavigator
 import io.novafoundation.nova.common.di.scope.ApplicationScope
-import io.novafoundation.nova.feature_account_impl.presentation.AccountRouter
-import io.novafoundation.nova.feature_account_impl.presentation.AdvancedEncryptionCommunicator
 import io.novafoundation.nova.feature_dapp_impl.DAppRouter
 
 @Module
--- feature-dapp-impl/src/main/java/io/novafoundation/nova/feature_dapp_impl/di/DappFeatureModule.kt
+++ feature-dapp-impl/src/main/java/io/novafoundation/nova/feature_dapp_impl/di/DappFeatureModule.kt
@@ -1,8 +1,6 @@
 package io.novafoundation.nova.feature_dapp_impl.di
 
 import dagger.Module
-import dagger.Provides
-import io.novafoundation.nova.feature_onboarding_api.domain.OnboardingInteractor
 
 @Module
 class DappFeatureModule
