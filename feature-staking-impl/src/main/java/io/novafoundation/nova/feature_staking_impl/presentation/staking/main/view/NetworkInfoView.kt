07693cca9a18ff1ebcccf02c6e7d403cc273636b
valentun <sergeev@soramitsu.co.jp>
2021-12-14 14:02:22 +0400

    Code style

--- feature-staking-impl/src/main/java/io/novafoundation/nova/feature_staking_impl/presentation/staking/main/view/NetworkInfoView.kt
+++ feature-staking-impl/src/main/java/io/novafoundation/nova/feature_staking_impl/presentation/staking/main/view/NetworkInfoView.kt
@@ -1,6 +1,5 @@
 package io.novafoundation.nova.feature_staking_impl.presentation.staking.main.view
 
-import android.animation.LayoutTransition
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
