5c23a367a2d2284f736b15abeb27d664bf9912e3
Stepan Lavrentev <lawrentievsv@gmail.com>
2021-12-24 17:02:45 +0300

    turn off logcat
    Signed-off-by: Stepan Lavrentev <lawrentievsv@gmail.com>

--- .github/workflows/networks_test.sh
+++ .github/workflows/networks_test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 adb devices
-adb logcat -c
-adb logcat &
+# adb logcat -c
+# adb logcat &
 ./gradlew :app:connectedDebugAndroidTest -Pandroid.testInstrumentationRunnerArguments.class=io.novafoundation.nova.balances.BalancesIntegrationTest
