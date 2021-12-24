e8a9f0ec36a832b8308d069de0dfe455750252e6
Stepan Lavrentev <lawrentievsv@gmail.com>
2021-12-24 14:10:11 +0300

    add logcat
    Signed-off-by: Stepan Lavrentev <lawrentievsv@gmail.com>

--- .github/workflows/networks_test.sh
+++ .github/workflows/networks_test.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 adb devices
+adb logcat -c
+adb logcat &
 ./gradlew :app:connectedDebugAndroidTest -Pandroid.testInstrumentationRunnerArguments.class=io.novafoundation.nova.balances.BalancesIntegrationTest
