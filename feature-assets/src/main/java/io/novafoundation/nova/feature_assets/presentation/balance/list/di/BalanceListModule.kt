b242048a68d2c63d4942b3a7836ab8187078439f
valentunn <70131744+valentunn@users.noreply.github.com>
2022-03-02 10:53:37 +0300

    Nft/previews (#169)
    * Add nft previews and counter to "Your nfts" button
    
    * Code style
    
    * Do not sync loaded nfts on previews
    
    * Code style
    
    * Add animations to navigation
    
    * Remove commented code

--- feature-assets/src/main/java/io/novafoundation/nova/feature_assets/presentation/balance/list/di/BalanceListModule.kt
+++ feature-assets/src/main/java/io/novafoundation/nova/feature_assets/presentation/balance/list/di/BalanceListModule.kt
@@ -7,21 +7,33 @@ import dagger.Module
 import dagger.Provides
 import dagger.multibindings.IntoMap
 import io.novafoundation.nova.common.address.AddressIconGenerator
+import io.novafoundation.nova.common.di.scope.ScreenScope
 import io.novafoundation.nova.common.di.viewmodel.ViewModelKey
 import io.novafoundation.nova.common.di.viewmodel.ViewModelModule
+import io.novafoundation.nova.feature_account_api.domain.interfaces.AccountRepository
 import io.novafoundation.nova.feature_account_api.domain.interfaces.SelectedAccountUseCase
 import io.novafoundation.nova.feature_assets.domain.WalletInteractor
+import io.novafoundation.nova.feature_assets.domain.assets.list.AssetsListInteractor
 import io.novafoundation.nova.feature_assets.presentation.WalletRouter
 import io.novafoundation.nova.feature_assets.presentation.balance.list.BalanceListViewModel
+import io.novafoundation.nova.feature_nft_api.data.repository.NftRepository
 
 @Module(includes = [ViewModelModule::class])
 class BalanceListModule {
+
+    @Provides
+    @ScreenScope
+    fun provideInteractor(
+        accountRepository: AccountRepository,
+        nftRepository: NftRepository
+    ) = AssetsListInteractor(accountRepository, nftRepository)
 
     @Provides
     @IntoMap
     @ViewModelKey(BalanceListViewModel::class)
     fun provideViewModel(
         interactor: WalletInteractor,
+        assetsListInteractor: AssetsListInteractor,
         router: WalletRouter,
         selectedAccountUseCase: SelectedAccountUseCase,
         addressIconGenerator: AddressIconGenerator,
@@ -29,6 +41,7 @@ class BalanceListModule {
 
         return BalanceListViewModel(
             interactor,
+            assetsListInteractor,
             addressIconGenerator,
             selectedAccountUseCase,
             router
