e121e1aea6c625ac2241a1fb2bd61d7c8e62f049
valentun <sergeev@soramitsu.co.jp>
2022-03-09 10:44:27 +0300

    Disable okhttp cache for rmrkV1 and rmrkV2 nft list requests

---
+++ common/src/main/java/io/novafoundation/nova/common/data/network/http/CacheControl.kt
@@ -0,0 +1,6 @@
+package io.novafoundation.nova.common.data.network.http
+
+object CacheControl {
+
+    const val NO_CACHE = "Cache-control: no-cache"
+}
--- feature-nft-impl/src/main/java/io/novafoundation/nova/feature_nft_impl/data/source/providers/rmrkV2/network/RmrkV2Api.kt
+++ feature-nft-impl/src/main/java/io/novafoundation/nova/feature_nft_impl/data/source/providers/rmrkV2/network/RmrkV2Api.kt
@@ -1,6 +1,8 @@
 package io.novafoundation.nova.feature_nft_impl.data.source.providers.rmrkV2.network
 
+import io.novafoundation.nova.common.data.network.http.CacheControl
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Path
 import retrofit2.http.Url
 
@@ -11,9 +13,11 @@ interface RmrkV2Api {
     }
 
     @GET("account-birds/{address}")
+    @Headers(CacheControl.NO_CACHE)
     suspend fun getBirds(@Path("address") address: String): List<RmrkV2NftRemote>
 
     @GET("account-items/{address}")
+    @Headers(CacheControl.NO_CACHE)
     suspend fun getItems(@Path("address") address: String): List<RmrkV2NftRemote>
 
     @GET
