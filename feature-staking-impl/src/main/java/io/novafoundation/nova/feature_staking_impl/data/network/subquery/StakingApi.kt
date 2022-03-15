b7bf7b85a92a951793f29b40f98db3643b97dbad
valentun <sergeev@soramitsu.co.jp>
2022-03-15 13:05:25 +0300

    Migrate to new total reward api

--- feature-staking-impl/src/main/java/io/novafoundation/nova/feature_staking_impl/data/network/subquery/StakingApi.kt
+++ feature-staking-impl/src/main/java/io/novafoundation/nova/feature_staking_impl/data/network/subquery/StakingApi.kt
@@ -2,9 +2,9 @@ package io.novafoundation.nova.feature_staking_impl.data.network.subquery
 
 import io.novafoundation.nova.common.data.network.subquery.EraValidatorInfoQueryResponse
 import io.novafoundation.nova.common.data.network.subquery.SubQueryResponse
-import io.novafoundation.nova.common.data.network.subquery.TransactionHistoryRemote
 import io.novafoundation.nova.feature_staking_impl.data.network.subquery.request.StakingEraValidatorInfosRequest
 import io.novafoundation.nova.feature_staking_impl.data.network.subquery.request.StakingSumRewardRequest
+import io.novafoundation.nova.feature_staking_impl.data.network.subquery.response.SubQueryTotalRewardResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
 import retrofit2.http.Url
@@ -12,10 +12,10 @@ import retrofit2.http.Url
 interface StakingApi {
 
     @POST
-    suspend fun getSumReward(
+    suspend fun getTotalReward(
         @Url url: String,
         @Body body: StakingSumRewardRequest
-    ): SubQueryResponse<TransactionHistoryRemote>
+    ): SubQueryResponse<SubQueryTotalRewardResponse>
 
     @POST
     suspend fun getValidatorsInfo(
