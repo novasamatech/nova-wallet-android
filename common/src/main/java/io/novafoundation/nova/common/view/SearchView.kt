b89ebd956fbbebbc96e9d332abd223796eb0ae91
valentun <sergeev@soramitsu.co.jp>
2022-03-18 14:53:59 +0300

    Redesign search custom validators

--- common/src/main/java/io/novafoundation/nova/common/view/SearchView.kt
+++ common/src/main/java/io/novafoundation/nova/common/view/SearchView.kt
@@ -1,14 +1,18 @@
-package io.novafoundation.nova.feature_dapp_impl.presentation.search
+package io.novafoundation.nova.common.view
 
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.EditText
 import android.widget.LinearLayout
+import androidx.annotation.DrawableRes
+import io.novafoundation.nova.common.R
 import io.novafoundation.nova.common.utils.WithContextExtensions
+import io.novafoundation.nova.common.utils.getResourceIdOrNull
 import io.novafoundation.nova.common.utils.onTextChanged
+import io.novafoundation.nova.common.utils.setDrawableStart
 import io.novafoundation.nova.common.utils.setVisible
-import io.novafoundation.nova.feature_dapp_impl.R
+import io.novafoundation.nova.common.utils.useAttributes
 import kotlinx.android.synthetic.main.view_search.view.searchClear
 import kotlinx.android.synthetic.main.view_search.view.searchContent
 
@@ -24,6 +28,14 @@ class SearchView @JvmOverloads constructor(
     val content: EditText
         get() = searchContent
 
+    fun setHint(hint: String?) {
+        content.hint = hint
+    }
+
+    fun setIcon(@DrawableRes icon: Int?) {
+        searchContent.setDrawableStart(icon, widthInDp = 16, paddingInDp = 6, tint = R.color.white_32)
+    }
+
     init {
         View.inflate(context, R.layout.view_search, this)
 
@@ -37,5 +49,15 @@ class SearchView @JvmOverloads constructor(
         searchClear.setOnClickListener {
             content.text.clear()
         }
+
+        attrs?.let(::applyAttrs)
+    }
+
+    private fun applyAttrs(attributeSet: AttributeSet) = context.useAttributes(attributeSet, R.styleable.SearchView) {
+        val hint = it.getString(R.styleable.SearchView_android_hint)
+        setHint(hint)
+
+        val icon = it.getResourceIdOrNull(R.styleable.SearchView_icon)
+        setIcon(icon)
     }
 }
