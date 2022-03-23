f02bda0c9016f18fd78679352e4177382ad5170b
valentun <sergeev@soramitsu.co.jp>
2022-03-23 13:50:22 +0300

    Redesign redeem

--- feature-staking-impl/src/main/java/io/novafoundation/nova/feature_staking_impl/presentation/staking/redeem/RedeemFragment.kt
+++ feature-staking-impl/src/main/java/io/novafoundation/nova/feature_staking_impl/presentation/staking/redeem/RedeemFragment.kt
@@ -4,21 +4,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import dev.chrisbanes.insetter.applyInsetter
 import io.novafoundation.nova.common.base.BaseFragment
 import io.novafoundation.nova.common.di.FeatureUtils
 import io.novafoundation.nova.common.mixin.impl.observeRetries
 import io.novafoundation.nova.common.mixin.impl.observeValidations
-import io.novafoundation.nova.common.view.setProgress
+import io.novafoundation.nova.common.utils.applyStatusBarInsets
 import io.novafoundation.nova.feature_account_api.presenatation.actions.setupExternalActions
 import io.novafoundation.nova.feature_staking_api.di.StakingFeatureApi
 import io.novafoundation.nova.feature_staking_impl.R
 import io.novafoundation.nova.feature_staking_impl.di.StakingFeatureComponent
+import io.novafoundation.nova.feature_wallet_api.presentation.mixin.fee.setupFeeLoading
 import kotlinx.android.synthetic.main.fragment_redeem.redeemAmount
 import kotlinx.android.synthetic.main.fragment_redeem.redeemConfirm
 import kotlinx.android.synthetic.main.fragment_redeem.redeemContainer
-import kotlinx.android.synthetic.main.fragment_redeem.redeemFee
-import kotlinx.android.synthetic.main.fragment_redeem.redeemOriginAccount
+import kotlinx.android.synthetic.main.fragment_redeem.redeemExtrinsicInformation
 import kotlinx.android.synthetic.main.fragment_redeem.redeemToolbar
 
 private const val PAYLOAD_KEY = "PAYLOAD_KEY"
@@ -41,18 +40,13 @@ class RedeemFragment : BaseFragment<RedeemViewModel>() {
     }
 
     override fun initViews() {
-        redeemContainer.applyInsetter {
-            type(statusBars = true) {
-                padding()
-            }
-
-            consume(true)
-        }
+        redeemContainer.applyStatusBarInsets()
 
         redeemToolbar.setHomeButtonListener { viewModel.backClicked() }
         redeemConfirm.prepareForProgress(viewLifecycleOwner)
         redeemConfirm.setOnClickListener { viewModel.confirmClicked() }
-        redeemOriginAccount.setWholeClickListener { viewModel.originAccountClicked() }
+
+        redeemExtrinsicInformation.setOnAccountClickedListener { viewModel.originAccountClicked() }
     }
 
     override fun inject() {
@@ -71,25 +65,12 @@ class RedeemFragment : BaseFragment<RedeemViewModel>() {
         observeRetries(viewModel)
         observeValidations(viewModel)
         setupExternalActions(viewModel)
+        setupFeeLoading(viewModel, redeemExtrinsicInformation.fee)
 
-        viewModel.showNextProgress.observe(redeemConfirm::setProgress)
+        viewModel.amountModelFlow.observe(redeemAmount::setAmount)
 
-        viewModel.assetModelLiveData.observe {
-            redeemAmount.setAssetBalance(it.assetBalance)
-            redeemAmount.setAssetName(it.tokenName)
-            redeemAmount.loadAssetImage(it.imageUrl)
-        }
-
-        viewModel.amountLiveData.observe { (amount, fiatAmount) ->
-            redeemAmount.amountInput.setText(amount)
-            fiatAmount.let(redeemAmount::setFiatAmount)
-        }
-
-        viewModel.originAddressModelLiveData.observe {
-            redeemOriginAccount.setMessage(it.nameOrAddress)
-            redeemOriginAccount.setTextIcon(it.image)
-        }
-
-        viewModel.feeLiveData.observe(redeemFee::setFeeStatus)
+        viewModel.walletUiFlow.observe(redeemExtrinsicInformation::setWallet)
+        viewModel.feeLiveData.observe(redeemExtrinsicInformation::setFeeStatus)
+        viewModel.originAddressModelFlow.observe(redeemExtrinsicInformation::setAccount)
     }
 }
