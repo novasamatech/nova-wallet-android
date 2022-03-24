7d053f72f465edda04c9d3b0bdf82d7c84ea5ab8
valentun <sergeev@soramitsu.co.jp>
2022-03-24 15:24:57 +0300

    Redesign confirm payout

--- feature-wallet-api/src/main/java/io/novafoundation/nova/feature_wallet_api/presentation/mixin/fee/FeeUI.kt
+++ feature-wallet-api/src/main/java/io/novafoundation/nova/feature_wallet_api/presentation/mixin/fee/FeeUI.kt
@@ -1,20 +1,10 @@
 package io.novafoundation.nova.feature_wallet_api.presentation.mixin.fee
 
-import android.widget.ProgressBar
-import android.widget.TextView
 import io.novafoundation.nova.common.base.BaseFragmentMixin
 import io.novafoundation.nova.common.base.BaseViewModel
 import io.novafoundation.nova.common.mixin.impl.observeRetries
 import io.novafoundation.nova.common.utils.makeGone
-import io.novafoundation.nova.common.utils.setVisible
-import io.novafoundation.nova.feature_wallet_api.R
 import io.novafoundation.nova.feature_wallet_api.presentation.view.FeeView
-
-class FeeViews(
-    val progress: ProgressBar,
-    val fiat: TextView,
-    val token: TextView,
-)
 
 interface WithFeeLoaderMixin {
 
@@ -37,37 +27,4 @@ fun BaseFragmentMixin<*>.setupFeeLoading(withFeeLoaderMixin: WithFeeLoaderMixin,
     } else {
         feeView.makeGone()
     }
-}
-
-fun displayFeeStatus(
-    feeStatus: FeeStatus,
-    feeViews: FeeViews,
-) = with(feeViews) {
-    val context = progress.context
-
-    when (feeStatus) {
-        is FeeStatus.Loading -> feeProgressShown(true, feeViews)
-        is FeeStatus.Error -> {
-            feeProgressShown(false, feeViews)
-
-            token.text = context.getString(R.string.common_error_general_title)
-            fiat.text = ""
-        }
-        is FeeStatus.Loaded -> {
-            feeProgressShown(false, feeViews)
-
-            fiat.text = feeStatus.feeModel.displayFiat
-            token.text = feeStatus.feeModel.displayToken
-        }
-    }
-}
-
-private fun feeProgressShown(
-    shown: Boolean,
-    feeViews: FeeViews,
-) = with(feeViews) {
-    fiat.setVisible(!shown)
-    token.setVisible(!shown)
-
-    progress.setVisible(shown)
 }
