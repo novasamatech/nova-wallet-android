54e08de5c542fd62ac15e2cd2fc1cbcc04564c62
Stepan Lavrentev <lawrentievsv@gmail.com>
2022-04-26 14:01:07 +0300

    Fix test result exporting

--- .github/workflows/networks_test.sh
+++ .github/workflows/networks_test.sh
@@ -2,7 +2,15 @@
 adb devices
 # adb logcat -c
 # adb logcat &
-./gradlew :app:connectedDebugAndroidTest -Pandroid.testInstrumentationRunnerArguments.class=io.novafoundation.nova.balances.BalancesIntegrationTest
 
-# download results
+# Build tests
+./gradlew :app:assembleDebugAndroidTest
+
+# Run tests
+adb shell am instrument -w -m -e debug false -e class 'io.novafoundation.nova.balances.BalancesIntegrationTest' io.novafoundation.nova.debug.test/io.qameta.allure.android.runners.AllureAndroidJUnitRunner
+EXIT_STATUS=$?
+
+# Export results
 adb exec-out run-as io.novafoundation.nova.debug sh -c 'cd /data/data/io.novafoundation.nova.debug/files && tar cf - allure-results' > allure-results.tar
+
+exit $EXIT_STATUS
