e8bc1991b8ae1e10cfcaf4019b9d7e5b8559f0b0
valentun <sergeev@soramitsu.co.jp>
2022-07-13 19:18:33 +0700

    Code style

--- feature-crowdloan-impl/src/main/java/io/novafoundation/nova/feature_crowdloan_impl/domain/contributions/Contribution.kt
+++ feature-crowdloan-impl/src/main/java/io/novafoundation/nova/feature_crowdloan_impl/domain/contributions/Contribution.kt
@@ -5,7 +5,6 @@ import io.novafoundation.nova.common.utils.formatting.TimerValue
 import io.novafoundation.nova.feature_crowdloan_api.data.network.blockhain.binding.FundInfo
 import io.novafoundation.nova.feature_crowdloan_api.data.repository.ParachainMetadata
 import java.math.BigInteger
-import kotlin.time.ExperimentalTime
 
 class Contribution(
     val amount: BigInteger,
