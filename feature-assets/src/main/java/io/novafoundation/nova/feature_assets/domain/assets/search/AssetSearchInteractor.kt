74ab29f11bbff1872ef26bc2f0c9e7a9bbc897d1
valentun <sergeev@soramitsu.co.jp>
2022-07-14 14:20:37 +0700

    Inlcude non full token matches to search results. Optimize match checks

--- feature-assets/src/main/java/io/novafoundation/nova/feature_assets/domain/assets/search/AssetSearchInteractor.kt
+++ feature-assets/src/main/java/io/novafoundation/nova/feature_assets/domain/assets/search/AssetSearchInteractor.kt
@@ -14,15 +14,26 @@ import kotlinx.coroutines.flow.flatMapLatest
 
 private class SearchResult(
     val item: Asset,
-    val fullMatch: Boolean,
-    val prefixMatch: Boolean
+    val match: Match
 )
+
+private enum class Match {
+    NONE, INCLUSION, PREFIX, FULL;
+}
+
+private val Match.matchFound
+    get() = this != Match.NONE
+private val Match.isFullMatch
+    get() = this == Match.FULL
+private val Match.atLeastPrefixMatch
+    get() = this >= Match.PREFIX
 
 class AssetSearchInteractor(
     private val walletRepository: WalletRepository,
     private val accountRepository: AccountRepository,
     private val chainRegistry: ChainRegistry,
 ) {
+
 
     fun searchAssetsFlow(
         queryFlow: Flow<String>
@@ -48,25 +59,32 @@ class AssetSearchInteractor(
         val searchResultsFromTokens = map {
             SearchResult(
                 item = it,
-                fullMatch = it.token.configuration.symbol fullMatch query,
-                prefixMatch = it.token.configuration.symbol prefixMatch query
+                match = it.token.configuration.symbol match query
             )
         }
-        val allFullMatchesFromTokens = searchResultsFromTokens.filter { it.fullMatch }
+        val anyMatchFromTokens = searchResultsFromTokens.mapNotNull { searchResult ->
+            searchResult.item.takeIf { searchResult.match.matchFound }
+        }
+
+        val allFullMatchesFromTokens = searchResultsFromTokens.filter { it.match.isFullMatch }
         if (allFullMatchesFromTokens.isNotEmpty()) {
-            return allFullMatchesFromTokens.map(SearchResult::item)
+            return anyMatchFromTokens
         }
 
         val foundChainIds = chainsById.values.filter { it.name inclusionMatch query }
             .map(Chain::id)
             .toSet()
 
-        val fromTokenSearch = searchResultsFromTokens.mapNotNull { searchResult ->
-            searchResult.item.takeIf { searchResult.prefixMatch }
-        }
         val fromChainSearch = filter { it.token.configuration.chainId in foundChainIds }
 
-        return (fromTokenSearch + fromChainSearch).distinct()
+        return (anyMatchFromTokens + fromChainSearch).distinct()
+    }
+
+    private infix fun String.match(query: String): Match = when {
+        fullMatch(query) -> Match.FULL
+        prefixMatch(prefix = query) -> Match.PREFIX
+        inclusionMatch(inclusion = query) -> Match.INCLUSION
+        else -> Match.NONE
     }
 
     @OptIn(ExperimentalStdlibApi::class)
