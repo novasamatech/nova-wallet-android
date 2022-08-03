14f6e7eb9010a14d5b69f3d14b117a20f5b23328
valentun <sergeev@soramitsu.co.jp>
2022-08-03 15:45:42 +0300

    Make DApp sign a full screen to allow parity signer flow to open

--- feature-dapp-impl/src/main/java/io/novafoundation/nova/feature_dapp_impl/presentation/browser/signExtrinsic/DAppSignFragment.kt
+++ feature-dapp-impl/src/main/java/io/novafoundation/nova/feature_dapp_impl/presentation/browser/signExtrinsic/DAppSignFragment.kt
@@ -1,16 +1,14 @@
 package io.novafoundation.nova.feature_dapp_impl.presentation.browser.signExtrinsic
 
-import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
 import coil.ImageLoader
-import io.novafoundation.nova.common.base.BaseBottomSheetFragment
+import io.novafoundation.nova.common.base.BaseFragment
 import io.novafoundation.nova.common.di.FeatureUtils
 import io.novafoundation.nova.common.mixin.impl.observeValidations
-import io.novafoundation.nova.common.utils.inflateChild
 import io.novafoundation.nova.common.utils.makeGone
 import io.novafoundation.nova.common.utils.postToSelf
 import io.novafoundation.nova.common.view.setProgress
@@ -24,7 +22,6 @@ import io.novafoundation.nova.feature_dapp_impl.presentation.common.showDAppIcon
 import io.novafoundation.nova.feature_wallet_api.presentation.mixin.fee.setupFeeLoading
 import kotlinx.android.synthetic.main.bottom_sheet_confirm_dapp_action.confirmDAppActionAllow
 import kotlinx.android.synthetic.main.bottom_sheet_confirm_dapp_action.confirmDAppActionReject
-import kotlinx.android.synthetic.main.bottom_sheet_confirm_dapp_action.confirmInnerContent
 import kotlinx.android.synthetic.main.bottom_sheet_confirm_sign_extrinsic.confirmSignExtinsicAccount
 import kotlinx.android.synthetic.main.bottom_sheet_confirm_sign_extrinsic.confirmSignExtinsicDetails
 import kotlinx.android.synthetic.main.bottom_sheet_confirm_sign_extrinsic.confirmSignExtinsicFee
@@ -35,7 +32,7 @@ import javax.inject.Inject
 
 private const val PAYLOAD_KEY = "DAppSignExtrinsicFragment.Payload"
 
-class DAppSignExtrinsicFragment : BaseBottomSheetFragment<DAppSignViewModel>() {
+class DAppSignExtrinsicFragment : BaseFragment<DAppSignViewModel>() {
 
     companion object {
 
@@ -50,11 +47,11 @@ class DAppSignExtrinsicFragment : BaseBottomSheetFragment<DAppSignViewModel>() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return layoutInflater.inflate(R.layout.bottom_sheet_confirm_dapp_action, container, false)
+        return layoutInflater.inflate(R.layout.bottom_sheet_confirm_sign_extrinsic, container, false)
     }
 
     override fun initViews() {
-        confirmInnerContent.inflateChild(R.layout.bottom_sheet_confirm_sign_extrinsic, attachToRoot = true)
+        onBackPressed { viewModel.cancelled() }
 
         confirmDAppActionAllow.prepareForProgress(viewLifecycleOwner)
 
@@ -63,10 +60,6 @@ class DAppSignExtrinsicFragment : BaseBottomSheetFragment<DAppSignViewModel>() {
         confirmDAppActionReject.setOnClickListener { viewModel.rejectClicked() }
 
         confirmSignExtinsicDetails.setOnClickListener { viewModel.detailsClicked() }
-    }
-
-    override fun onCancel(dialog: DialogInterface) {
-        viewModel.cancelled()
     }
 
     override fun inject() {
@@ -106,7 +99,6 @@ class DAppSignExtrinsicFragment : BaseBottomSheetFragment<DAppSignViewModel>() {
         viewModel.performingOperationInProgress.observe { operationInProgress ->
             val actionsAllowed = !operationInProgress
 
-            isCancelable = actionsAllowed
             confirmDAppActionReject.isEnabled = actionsAllowed
             confirmDAppActionAllow.setProgress(show = operationInProgress)
         }
