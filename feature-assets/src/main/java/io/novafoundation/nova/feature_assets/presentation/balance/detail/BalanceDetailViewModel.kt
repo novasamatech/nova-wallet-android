4e8c5c60359114a08b582026b32139781dcac71f
antonijzelinskij <107959809+antonijzelinskij@users.noreply.github.com>
2022-08-19 11:56:42 +0300

    Add reserved balance (#388)
    * Add reserved balance to balance locks

--- feature-assets/src/main/java/io/novafoundation/nova/feature_assets/presentation/balance/detail/BalanceDetailViewModel.kt
+++ feature-assets/src/main/java/io/novafoundation/nova/feature_assets/presentation/balance/detail/BalanceDetailViewModel.kt
@@ -71,11 +71,6 @@ class BalanceDetailViewModel(
         .inBackground()
         .share()
 
-    val lockedBalanceAvailability = lockedBalanceModel
-        .map { isBalanceLocksAvailable(it) }
-        .inBackground()
-        .share()
-
     val buyMixin = buyMixinFactory.create(scope = this, assetPayload)
 
     val sendEnabled = assetFlow.map {
@@ -132,13 +127,7 @@ class BalanceDetailViewModel(
 
     fun lockedInfoClicked() = launch {
         val balanceLocks = lockedBalanceModel.first()
-        if (balanceLocks != null) {
-            _showLockedDetailsEvent.value = Event(balanceLocks)
-        }
-    }
-
-    private fun isBalanceLocksAvailable(balanceLocks: BalanceLocksModel?): Boolean {
-        return balanceLocks != null
+        _showLockedDetailsEvent.value = Event(balanceLocks)
     }
 
     private fun requireSecretsWallet(action: () -> Unit) {
@@ -161,25 +150,34 @@ class BalanceDetailViewModel(
         )
     }
 
-    private fun mapBalanceLocksToUi(balanceLocks: BalanceLocks?, asset: Asset): BalanceLocksModel? {
-        if (balanceLocks == null) return null
+    @OptIn(ExperimentalStdlibApi::class)
+    private fun mapBalanceLocksToUi(balanceLocks: BalanceLocks?, asset: Asset): BalanceLocksModel {
+        val mappedLocks = balanceLocks?.locks?.map {
+            BalanceLocksModel.Lock(
+                mapBalanceLockIdToUi(it.id),
+                mapAmountToAmountModel(it.amount, asset)
+            )
+        }
+
+        val reservedBalance = BalanceLocksModel.Lock(
+            resourceManager.getString(R.string.assets_balance_details_locks_reserved),
+            mapAmountToAmountModel(asset.reserved, asset)
+        )
 
         return BalanceLocksModel(
-            balanceLocks.locks.map {
-                BalanceLocksModel.Lock(
-                    mapBalanceLockIdToUi(it.id),
-                    mapAmountToAmountModel(it.amount, asset)
-                )
+            buildList {
+                mappedLocks?.let { addAll(it) }
+                add(reservedBalance)
             }
         )
     }
 
     private fun mapBalanceLockIdToUi(id: String): String {
         return when (id) {
-            "staking" -> resourceManager.getString(R.string.wallet_balance_locks_staking)
-            "democrac" -> resourceManager.getString(R.string.wallet_balance_locks_democrac)
-            "vesting" -> resourceManager.getString(R.string.wallet_balance_locks_vesting)
-            "phrelect" -> resourceManager.getString(R.string.wallet_balance_locks_phrelect)
+            "staking" -> resourceManager.getString(R.string.assets_balance_details_locks_staking)
+            "democrac" -> resourceManager.getString(R.string.assets_balance_details_locks_democrac)
+            "vesting" -> resourceManager.getString(R.string.assets_balance_details_locks_vesting)
+            "phrelect" -> resourceManager.getString(R.string.assets_balance_details_locks_phrelect)
             else -> id.capitalize(Locale.getDefault())
         }
     }
