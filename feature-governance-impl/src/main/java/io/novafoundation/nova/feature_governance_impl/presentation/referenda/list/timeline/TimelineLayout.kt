04696709a55b261839512fad71071c33c47881da
antonijzelinskij <antonijzelinskij@gmail.com>
2022-10-10 12:25:48 +0600

    Add saved state for timeline

--- feature-governance-impl/src/main/java/io/novafoundation/nova/feature_governance_impl/presentation/referenda/list/timeline/TimelineLayout.kt
+++ feature-governance-impl/src/main/java/io/novafoundation/nova/feature_governance_impl/presentation/referenda/list/timeline/TimelineLayout.kt
@@ -7,6 +7,8 @@ import android.graphics.DashPathEffect
 import android.graphics.Paint
 import android.graphics.Path
 import android.graphics.PointF
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.widget.LinearLayout
 import androidx.annotation.ColorRes
@@ -14,6 +16,7 @@ import androidx.annotation.DrawableRes
 import io.novafoundation.nova.common.utils.dpF
 import io.novafoundation.nova.common.utils.updatePadding
 import io.novafoundation.nova.feature_governance_impl.R
+import kotlinx.android.parcel.Parcelize
 import kotlin.math.roundToInt
 
 class TimelineLayout @JvmOverloads constructor(
@@ -29,8 +32,7 @@ class TimelineLayout @JvmOverloads constructor(
     private val dashFilledInterval: Float = 2.dpF(context)
     private val dashEmptyInterval: Float = 3.dpF(context)
 
-    private var finished: Boolean = true
-    private var timeline: List<TimelineState> = listOf()
+    private var timeline: Timeline = createDefaultTimeline()
 
     private var statePointSize: Float = 0f
     private var pointToStrokeOffset: Float = 0f
@@ -79,15 +81,7 @@ class TimelineLayout @JvmOverloads constructor(
         }
 
         orientation = VERTICAL
-        setTimeline(
-            listOf(
-                TimelineState("Title", "Subtitle", null, R.color.white),
-                TimelineState("Title", "Subtitle", null, R.color.white),
-                TimelineState("Title", "Subtitle", R.drawable.ic_time_16, R.color.yellow)
-            ),
-            true
-        )
-        
+
         setWillNotDraw(false)
     }
 
@@ -107,7 +101,7 @@ class TimelineLayout @JvmOverloads constructor(
 
         timelineStatePath.reset()
         if (timelineStatePoints.size > 1) {
-            val pathsCount = timeline.size - 1
+            val pathsCount = timeline.states.size - 1
             for (i in 0 until pathsCount) {
                 val pointStart = timelineStatePoints[i]
                 val pointEnd = timelineStatePoints[i + 1]
@@ -117,10 +111,26 @@ class TimelineLayout @JvmOverloads constructor(
         }
 
         timelineFinishedPath.reset()
-        if (finished && timelineStatePoints.isNotEmpty()) {
+        if (timeline.finished && timelineStatePoints.isNotEmpty()) {
             val lastPoint = timelineStatePoints.last()
             timelineFinishedPath.moveTo(lastPoint.x, lastPoint.y + totalOffsetFromPointCenter)
             timelineFinishedPath.lineTo(lastPoint.x, measuredHeight.toFloat())
+        }
+    }
+
+    override fun onSaveInstanceState(): Parcelable {
+        return SavedState(
+            super.onSaveInstanceState(),
+            timeline
+        )
+    }
+
+    override fun onRestoreInstanceState(state: Parcelable?) {
+        if (state is SavedState) {
+            super.onRestoreInstanceState(state.superState)
+            timeline = state.timeline ?: createDefaultTimeline()
+        } else {
+            super.onRestoreInstanceState(state)
         }
     }
 
@@ -133,16 +143,15 @@ class TimelineLayout @JvmOverloads constructor(
         canvas.drawPath(timelineFinishedPath, timelineUnfinishedPathPaint)
     }
 
-    fun setTimeline(timeline: List<TimelineState>, finished: Boolean) {
+    fun setTimeline(timeline: Timeline) {
         this.timeline = timeline
-        this.finished = finished
         removeAllViewsInLayout()
-        timeline.forEach { timelineState ->
+        timeline.states.forEach { timelineState ->
             val timelineItem = createTimelineDefaultItem(timelineState)
             timelineItem.updatePadding(start = itemStartPadding)
             addView(timelineItem)
         }
-        timelineStatePoints = List(timeline.size) { PointF() }
+        timelineStatePoints = List(timeline.states.size) { PointF() }
 
         requestLayout()
     }
@@ -153,10 +162,65 @@ class TimelineLayout @JvmOverloads constructor(
         return timeLineItem
     }
 
+    private fun createDefaultTimeline(): Timeline {
+        return Timeline(
+            listOf(),
+            true
+        )
+    }
+
+    @Parcelize
+    data class Timeline(
+        val states: List<TimelineState>,
+        val finished: Boolean
+    ) : Parcelable
+
+    @Parcelize
     data class TimelineState(
         val title: String,
         val subtitle: String,
         @DrawableRes val subtitleIconRes: Int?,
         @ColorRes val subtitleColor: Int
-    )
+    ) : Parcelable
+
+    private class SavedState : BaseSavedState {
+
+        val timeline: Timeline?
+
+        constructor(
+            superState: Parcelable?,
+            timeline: Timeline
+        ) : super(superState) {
+            this.timeline = timeline
+        }
+
+        constructor(parcel: Parcel) : this(parcel, null)
+
+        constructor(parcel: Parcel, loader: ClassLoader?) : super(parcel, loader) {
+            timeline = parcel.readParcelable(Timeline::class.java.classLoader)
+        }
+
+        override fun writeToParcel(out: Parcel, flags: Int) {
+            super.writeToParcel(out, flags)
+            out.writeParcelable(timeline, flags)
+        }
+
+        override fun describeContents(): Int {
+            return 0
+        }
+
+        companion object CREATOR : Parcelable.ClassLoaderCreator<SavedState> {
+            override fun createFromParcel(parcel: Parcel): SavedState {
+                return SavedState(parcel)
+            }
+
+            override fun createFromParcel(parcel: Parcel, classLoader: ClassLoader?): SavedState {
+                return SavedState(parcel, classLoader)
+            }
+
+            override fun newArray(size: Int): Array<SavedState?> {
+                return arrayOfNulls(size)
+            }
+        }
+    }
 }
