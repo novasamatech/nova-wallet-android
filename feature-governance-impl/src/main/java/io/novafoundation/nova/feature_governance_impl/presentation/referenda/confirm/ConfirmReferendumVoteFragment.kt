7d04bc1e37ea954c79b6249cea67c470dff622c7
antonijzelinskij <antonijzelinskij@gmail.com>
2022-10-20 13:42:25 +0600

    Fixed layout

--- feature-governance-impl/src/main/java/io/novafoundation/nova/feature_governance_impl/presentation/referenda/confirm/ConfirmReferendumVoteFragment.kt
+++ feature-governance-impl/src/main/java/io/novafoundation/nova/feature_governance_impl/presentation/referenda/confirm/ConfirmReferendumVoteFragment.kt
@@ -8,7 +8,6 @@ import io.novafoundation.nova.common.base.BaseFragment
 import io.novafoundation.nova.common.di.FeatureUtils
 import io.novafoundation.nova.common.mixin.impl.observeRetries
 import io.novafoundation.nova.common.mixin.impl.observeValidations
-import io.novafoundation.nova.common.utils.setDrawableStart
 import io.novafoundation.nova.common.view.setProgress
 import io.novafoundation.nova.feature_account_api.presenatation.actions.setupExternalActions
 import io.novafoundation.nova.feature_governance_api.di.GovernanceFeatureApi
@@ -17,7 +16,6 @@ import io.novafoundation.nova.feature_governance_impl.di.GovernanceFeatureCompon
 import io.novafoundation.nova.feature_wallet_api.presentation.mixin.fee.setupFeeLoading
 import kotlinx.android.synthetic.main.fragment_referendum_confirm_vote.confirmReferendumVoteConfirm
 import kotlinx.android.synthetic.main.fragment_referendum_confirm_vote.confirmReferendumVoteInformation
-import kotlinx.android.synthetic.main.fragment_referendum_confirm_vote.confirmReferendumVoteNote
 import kotlinx.android.synthetic.main.fragment_referendum_confirm_vote.confirmReferendumVoteToolbar
 
 class ConfirmReferendumVoteFragment : BaseFragment<ConfirmReferendumVoteViewModel>() {
@@ -31,7 +29,6 @@ class ConfirmReferendumVoteFragment : BaseFragment<ConfirmReferendumVoteViewMode
     }
 
     override fun initViews() {
-        confirmReferendumVoteNote.setDrawableStart(R.drawable.ic_star_filled, widthInDp = 16, paddingInDp = 8, tint = R.color.white_48)
         confirmReferendumVoteToolbar.setHomeButtonListener { viewModel.backClicked() }
         onBackPressed { viewModel.backClicked() }
 
