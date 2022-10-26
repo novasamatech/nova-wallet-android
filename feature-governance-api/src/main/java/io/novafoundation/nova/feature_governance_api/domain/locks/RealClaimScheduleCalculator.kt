df4f1f76da44c9c6fcbb47211caf2a3efef1624a
valentun <sergeev@soramitsu.co.jp>
2022-10-26 17:19:24 +0700

    Fixes and documentation

--- feature-governance-api/src/main/java/io/novafoundation/nova/feature_governance_api/domain/locks/RealClaimScheduleCalculator.kt
+++ feature-governance-api/src/main/java/io/novafoundation/nova/feature_governance_api/domain/locks/RealClaimScheduleCalculator.kt
@@ -61,6 +61,48 @@ class RealClaimScheduleCalculator(
         return referenda.getValue(referendumId).maxConvictionEnd(vote)
     }
 
+    /**
+     * Given the information about Voting (priors + active votes), statuses of referenda and TrackLocks
+     * Constructs the estimated claiming schedule.
+     * The schedule is exact when all involved referenda are completed. Only ongoing referenda' end time is estimateted
+     *
+     * The claiming schedule shows how much tokens will be unlocked and when.
+     * Schedule may consist of zero or one [ClaimSchedule.UnlockChunk.Claimable] chunk
+     * and zero or more [ClaimSchedule.UnlockChunk.Pending] chunks.
+     *
+     * [ClaimSchedule.UnlockChunk.Pending] chunks also provides a set of [ClaimSchedule.ClaimAction] actions
+     * needed to claim whole chunk.
+     *
+     * The algorithm itself consists of several parts
+     *
+     * 1. Determine individual unlocks
+     * This step is based on prior [Voting.Casting.prior] and [AccountVote.Standard] standard votes
+     * a. Each non-zero prior has a single individual unlock
+     * b. Each non-zero vote has a single individual unlock.
+     *    However, unlock time for votes is at least unlock time of corresponding prior.
+     *
+     * During this step we also determine the list of [ClaimAffect],
+     * which later gets translated to [ClaimSchedule.ClaimAction].
+     *
+     * 2. Combine all locks with the same unlock time into single lock
+     *  a. Result's amount is the maximum between combined locks
+     *  b. Result's affects is a concatenation of all affects from combined locks
+     *
+     * 3. Construct preliminary unlock schedule based on the following algorithm
+     *   a. Sort pairs from step (2) by descending [ClaimableLock.claimAt] order
+     *   b. For each item in the sorted list, find the difference between the biggest currently processed lock and item's amount
+     *   c. Since we start from the most far locks in the future, finding a positive difference means that
+     *   this difference is actually an entry in desired unlock schedule. Negative difference means that this unlock is
+     *   completely covered by future's unlock with bigger amount. Thus, we should discard it from the schedule and move its affects
+     *   to the currently known maximum lock in order to not to loose its actions when unlocking maximum lock.
+     *
+     * 4. Check which if unlocks are claimable and which are not by constructing [ClaimSchedule.UnlockChunk] based on [currentBlockNumber]
+     * 5. Fold all [ClaimSchedule.UnlockChunk] into single chunk.
+     * 6. Find a gap between [voting] and [trackLocks], which indicates an extra claimable amount
+     * 7. If gap exists, then we should add it to claimable chunk. We should also check if we should perform extra [ClaimSchedule.ClaimAction.Unlock]
+     *  for each track that is included in the gap. We do that by finding by checking which [ClaimSchedule.ClaimAction.Unlock] unlocks are already present
+     *  in claimable chunk's actions in order to not to do them twice.
+     */
     override fun estimateClaimSchedule(): ClaimSchedule {
         val castingVotes = voting.entries.mapNotNull { (trackId, voting) ->
             voting.castOrNull<Voting.Casting>()?.let { trackId to it }
@@ -143,7 +185,7 @@ class RealClaimScheduleCalculator(
                     // there is something to unlock at this point
                     result[at] = lock.copy(amount = unlockedAmount)
                 } else {
-                    // this lock is completely shadowed by later (in time) locks with greater values
+                    // this lock is completely shadowed by later (in time) lock with greater value
                     result.remove(at)
 
                     // but we want to keep its actions so we move it to the current known maximum that goes later in time
