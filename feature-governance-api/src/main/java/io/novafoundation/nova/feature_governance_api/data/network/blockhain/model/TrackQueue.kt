598a27cb0891a98f33734f88f3165c464165026f
valentun <sergeev@soramitsu.co.jp>
2022-11-01 16:18:43 +0700

    Use track queue size as maxSize

--- feature-governance-api/src/main/java/io/novafoundation/nova/feature_governance_api/data/network/blockhain/model/TrackQueue.kt
+++ feature-governance-api/src/main/java/io/novafoundation/nova/feature_governance_api/data/network/blockhain/model/TrackQueue.kt
@@ -1,21 +1,21 @@
 package io.novafoundation.nova.feature_governance_api.data.network.blockhain.model
 
-class TrackQueue(val referenda: List<ReferendumId>, val maxSize: Int) {
+class TrackQueue(val referenda: List<ReferendumId>) {
 
     class Position(val index: Int, val maxSize: Int)
 
     companion object;
 }
 
-fun TrackQueue.Companion.empty(maxSize: Int): TrackQueue = TrackQueue(emptyList(), maxSize = maxSize)
+fun TrackQueue.Companion.empty(): TrackQueue = TrackQueue(emptyList())
 
 fun TrackQueue.positionOf(referendumId: ReferendumId): TrackQueue.Position {
     return TrackQueue.Position(
         index = referenda.indexOf(referendumId) + 1,
-        maxSize = maxSize
+        maxSize = referenda.size
     )
 }
 
 fun TrackQueue?.orEmpty(): TrackQueue {
-    return this ?: TrackQueue.empty(maxSize = 0)
+    return this ?: TrackQueue.empty()
 }
