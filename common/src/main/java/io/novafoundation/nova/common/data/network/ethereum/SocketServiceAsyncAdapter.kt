ece54d158e1913360640cdd31893e0303d9441b3
valentunn <70131744+valentunn@users.noreply.github.com>
2022-11-10 13:19:51 +0700

    Evm/connection (#585)
    * Implement network service based on Nova sockets
    
    * Eth subscribe works
    
    * Correct test name
    
    * Code style

---
+++ common/src/main/java/io/novafoundation/nova/common/data/network/ethereum/SocketServiceAsyncAdapter.kt
@@ -0,0 +1,68 @@
+package io.novafoundation.nova.common.data.network.ethereum
+
+import io.reactivex.Observable
+import io.reactivex.subjects.BehaviorSubject
+import jp.co.soramitsu.fearless_utils.wsrpc.SocketService
+import jp.co.soramitsu.fearless_utils.wsrpc.request.DeliveryType
+import jp.co.soramitsu.fearless_utils.wsrpc.request.runtime.RuntimeRequest
+import jp.co.soramitsu.fearless_utils.wsrpc.response.RpcResponse
+import jp.co.soramitsu.fearless_utils.wsrpc.subscription.response.SubscriptionChange
+import kotlinx.coroutines.future.asDeferred
+import org.web3j.protocol.core.Request
+import org.web3j.protocol.core.Response
+import java.util.concurrent.CompletableFuture
+
+fun SocketService.executeRequestAsFuture(
+    request: RuntimeRequest,
+    deliveryType: DeliveryType = DeliveryType.AT_LEAST_ONCE,
+): CompletableFuture<RpcResponse> {
+    val future = RequestCancellableFuture<RpcResponse>()
+
+    val callback = object : SocketService.ResponseListener<RpcResponse> {
+        override fun onError(throwable: Throwable) {
+            future.completeExceptionally(throwable)
+        }
+
+        override fun onNext(response: RpcResponse) {
+            future.complete(response)
+        }
+    }
+
+    future.cancellable = executeRequest(request, deliveryType, callback)
+
+    return future
+}
+
+fun SocketService.subscribeAsObservable(
+    request: RuntimeRequest,
+    unsubscribeMethod: String
+): Observable<SubscriptionChange> {
+    val subject = BehaviorSubject.create<SubscriptionChange>()
+
+    val callback = object : SocketService.ResponseListener<SubscriptionChange> {
+        override fun onError(throwable: Throwable) {
+            subject.onError(throwable)
+        }
+
+        override fun onNext(response: SubscriptionChange) {
+            subject.onNext(response)
+        }
+    }
+
+    val cancellable = subscribe(request, callback, unsubscribeMethod)
+
+    return subject.doOnDispose { cancellable.cancel() }
+}
+
+private class RequestCancellableFuture<T> : CompletableFuture<T>() {
+
+    var cancellable: SocketService.Cancellable? = null
+
+    override fun cancel(mayInterruptIfRunning: Boolean): Boolean {
+        cancellable?.cancel()
+
+        return super.cancel(mayInterruptIfRunning)
+    }
+}
+
+suspend fun <S, T : Response<*>> Request<S, T>.sendSuspend(): T = sendAsync().asDeferred().await()
---
+++ common/src/main/java/io/novafoundation/nova/common/data/network/ethereum/WebSocketWeb3jService.kt
@@ -0,0 +1,77 @@
+package io.novafoundation.nova.common.data.network.ethereum
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.reactivex.BackpressureStrategy
+import io.reactivex.Flowable
+import jp.co.soramitsu.fearless_utils.wsrpc.SocketService
+import jp.co.soramitsu.fearless_utils.wsrpc.request.runtime.RuntimeRequest
+import org.web3j.protocol.ObjectMapperFactory
+import org.web3j.protocol.Web3jService
+import org.web3j.protocol.core.BatchRequest
+import org.web3j.protocol.core.BatchResponse
+import org.web3j.protocol.core.Request
+import org.web3j.protocol.core.Response
+import org.web3j.protocol.websocket.WebSocketService
+import org.web3j.protocol.websocket.events.Notification
+import java.io.IOException
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.ExecutionException
+
+class WebSocketWeb3jService(
+    private val socketService: SocketService,
+    private val jsonMapper: ObjectMapper = ObjectMapperFactory.getObjectMapper()
+) : Web3jService {
+
+    /**
+     * Implementation based on [WebSocketService.send]
+     */
+    override fun <T : Response<*>?> send(request: Request<*, out Response<*>>, responseType: Class<T>): T {
+        return try {
+            sendAsync(request, responseType).get()
+        } catch (e: InterruptedException) {
+            Thread.interrupted()
+            throw IOException("Interrupted WebSocket request", e)
+        } catch (e: ExecutionException) {
+            if (e.cause is IOException) {
+                throw e.cause as IOException
+            }
+            throw RuntimeException("Unexpected exception", e.cause)
+        }
+    }
+
+    override fun <T : Response<*>?> sendAsync(request: Request<*, out Response<*>>, responseType: Class<T>): CompletableFuture<T> {
+        val runtimeRequest = request.toRuntimeRequest()
+
+        return socketService.executeRequestAsFuture(runtimeRequest).thenApply {
+            jsonMapper.convertValue(it, responseType)
+        }
+    }
+
+    override fun <T : Notification<*>?> subscribe(
+        request: Request<*, out Response<*>>,
+        unsubscribeMethod: String,
+        responseType: Class<T>
+    ): Flowable<T> {
+        val runtimeRequest = request.toRuntimeRequest()
+
+        return socketService.subscribeAsObservable(runtimeRequest, unsubscribeMethod).map {
+            jsonMapper.convertValue(it, responseType)
+        }.toFlowable(BackpressureStrategy.LATEST)
+    }
+
+    override fun sendBatch(batchRequest: BatchRequest): BatchResponse {
+        TODO("Batches not yet supported")
+    }
+
+    override fun sendBatchAsync(batchRequest: BatchRequest): CompletableFuture<BatchResponse> {
+        TODO("Batches not yet supported")
+    }
+
+    override fun close() {
+        // other components handle lifecycle of socketService
+    }
+
+    private fun Request<*, *>.toRuntimeRequest(): RuntimeRequest {
+        return RuntimeRequest(method = method, params = params, id = id.toInt())
+    }
+}
