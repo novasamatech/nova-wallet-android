4124631c68058fb4a764128b1a95207248fcdd2d
valentun <sergeev@soramitsu.co.jp>
2022-11-17 12:29:16 +0700

    Fix tests

--- runtime/src/main/java/io/novafoundation/nova/runtime/multiNetwork/chain/remote/model/ChainExternalApiRemote.kt
+++ runtime/src/main/java/io/novafoundation/nova/runtime/multiNetwork/chain/remote/model/ChainExternalApiRemote.kt
@@ -1,6 +1,6 @@
 package io.novafoundation.nova.runtime.multiNetwork.chain.remote.model
 
-class ChainExternalApiRemote(
+data class ChainExternalApiRemote(
     val staking: Section?,
     val history: List<TransferApi>,
     val crowdloans: Section?,
@@ -9,7 +9,7 @@ class ChainExternalApiRemote(
 
     class Section(val type: String, val url: String)
 
-    class TransferApi(
+    data class TransferApi(
         val type: String,
         val url: String,
         val assetType: String?
