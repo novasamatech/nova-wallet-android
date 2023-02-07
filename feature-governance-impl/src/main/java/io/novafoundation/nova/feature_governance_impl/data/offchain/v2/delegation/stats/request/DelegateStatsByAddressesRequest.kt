0f6c7c546ea4e09da7e1051b0c334e17fc6d06de
antonijzelinskij <antonijzelinskij@gmail.com>
2023-02-07 11:57:13 +0500

    Run ktlint

--- feature-governance-impl/src/main/java/io/novafoundation/nova/feature_governance_impl/data/offchain/v2/delegation/stats/request/DelegateStatsByAddressesRequest.kt
+++ feature-governance-impl/src/main/java/io/novafoundation/nova/feature_governance_impl/data/offchain/v2/delegation/stats/request/DelegateStatsByAddressesRequest.kt
@@ -2,7 +2,7 @@ package io.novafoundation.nova.feature_governance_impl.data.offchain.v2.delegati
 
 import io.novafoundation.nova.common.data.network.runtime.binding.BlockNumber
 
-class DelegateStatsByAddressesRequest(recentVotesBlockThreshold: BlockNumber, val addresses: List<String>)  {
+class DelegateStatsByAddressesRequest(recentVotesBlockThreshold: BlockNumber, val addresses: List<String>) {
     val query = """
         query {
            delegates(filter:{accountId:{in:[${getAddresses()}]}}) {
