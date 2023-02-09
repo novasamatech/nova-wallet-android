7049902a0ac09e61392a16913a9a683c31a3479c
antonijzelinskij <antonijzelinskij@gmail.com>
2023-02-09 17:44:44 +0500

    Run ktlint

--- common/src/main/java/io/novafoundation/nova/common/view/TableView.kt
+++ common/src/main/java/io/novafoundation/nova/common/view/TableView.kt
@@ -5,7 +5,6 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
-import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.view.children
