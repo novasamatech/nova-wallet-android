1a8c681f3484aa3ddccaa9ee7eb99da25f04d6e7
antonijzelinskij <107959809+antonijzelinskij@users.noreply.github.com>
2023-02-16 17:47:33 +0500

    Fix/delegation voters optimization (#757)
    * Referendum voters — UI
    
    * Fixed pr notes
    
    * Fix crash
    
    * Optimized address generation

---
+++ feature-governance-impl/src/main/java/io/novafoundation/nova/feature_governance_impl/presentation/referenda/voters/di/CachingDelegateMappers.kt
@@ -0,0 +1,7 @@
+package io.novafoundation.nova.feature_governance_impl.presentation.referenda.voters.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class CachingDelegateMappers
