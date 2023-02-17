52a939b37d6d24a19623c4295a0c5485aac8ece9
antonijzelinskij <107959809+antonijzelinskij@users.noreply.github.com>
2023-02-17 17:10:39 +0500

    Fixed crash (#759)
    * Fixed crash
    
    * Fixed test code

--- feature-versions-api/src/main/java/io/novafoundation/nova/feature_versions_api/domain/UpdateNotificationsInteractor.kt
+++ feature-versions-api/src/main/java/io/novafoundation/nova/feature_versions_api/domain/UpdateNotificationsInteractor.kt
@@ -11,4 +11,6 @@ interface UpdateNotificationsInteractor {
     suspend fun getUpdateNotifications(): List<UpdateNotification>
 
     suspend fun skipNewUpdates()
+
+    suspend fun loadVersions()
 }
