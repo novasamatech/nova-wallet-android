67e76770e252d0ab7625a3aff749fdd135aa4554
antonijzelinskij <antonijzelinskij@gmail.com>
2023-02-27 17:50:55 +0500

    Map locks for gov1 and gov2

--- feature-wallet-api/src/main/java/io/novafoundation/nova/feature_wallet_api/presentation/formatters/BalanceIdMapper.kt
+++ feature-wallet-api/src/main/java/io/novafoundation/nova/feature_wallet_api/presentation/formatters/BalanceIdMapper.kt
@@ -7,7 +7,8 @@ import io.novafoundation.nova.feature_wallet_api.R
 fun mapBalanceIdToUi(resourceManager: ResourceManager, id: String): String {
     return when (id.trim()) {
         "staking" -> resourceManager.getString(R.string.assets_balance_details_locks_staking)
-        "democrac", "pyconvot" -> resourceManager.getString(R.string.assets_balance_details_locks_democrac)
+        "democrac" -> resourceManager.getString(R.string.assets_balance_details_locks_democrac_v1)
+        "pyconvot" -> resourceManager.getString(R.string.assets_balance_details_locks_democrac_v2)
         "vesting" -> resourceManager.getString(R.string.assets_balance_details_locks_vesting)
         "phrelect" -> resourceManager.getString(R.string.assets_balance_details_locks_phrelect)
         "reserved" -> resourceManager.getString(R.string.wallet_balance_reserved)
