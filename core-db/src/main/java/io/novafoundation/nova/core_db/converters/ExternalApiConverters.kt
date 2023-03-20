1e6cf21db02267fa647303ae28e2c6ee1dcc645f
Valentun <vallentunn@gmail.com>
2023-03-20 16:08:21 +0700

    Forward compatibility for external api types

--- core-db/src/main/java/io/novafoundation/nova/core_db/converters/ExternalApiConverters.kt
+++ core-db/src/main/java/io/novafoundation/nova/core_db/converters/ExternalApiConverters.kt
@@ -1,17 +1,18 @@
 package io.novafoundation.nova.core_db.converters
 
 import androidx.room.TypeConverter
-import io.novafoundation.nova.core_db.model.chain.ChainExternalApiLocal
+import io.novafoundation.nova.common.utils.enumValueOfOrNull
+import io.novafoundation.nova.core_db.model.chain.ChainExternalApiLocal.SourceType
 
 class ExternalApiConverters {
 
     @TypeConverter
-    fun fromApiType(apiType: ChainExternalApiLocal.SourceType): String {
+    fun fromApiType(apiType: SourceType): String {
         return apiType.name
     }
 
     @TypeConverter
-    fun toApiType(raw: String): ChainExternalApiLocal.SourceType {
-        return enumValueOf(raw)
+    fun toApiType(raw: String): SourceType {
+        return enumValueOfOrNull<SourceType>(raw) ?: SourceType.UNKNOWN
     }
 }
