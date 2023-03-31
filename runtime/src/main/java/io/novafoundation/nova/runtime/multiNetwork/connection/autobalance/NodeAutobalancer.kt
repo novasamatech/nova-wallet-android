df3bda55a141c3469da2d1177056870d7052661a
Valentun <vallentunn@gmail.com>
2023-03-31 17:22:48 +0700

    Switch node when rate limit rpc error code is received

--- runtime/src/main/java/io/novafoundation/nova/runtime/multiNetwork/connection/autobalance/NodeAutobalancer.kt
+++ runtime/src/main/java/io/novafoundation/nova/runtime/multiNetwork/connection/autobalance/NodeAutobalancer.kt
@@ -3,12 +3,10 @@ package io.novafoundation.nova.runtime.multiNetwork.connection.autobalance
 import io.novafoundation.nova.runtime.multiNetwork.chain.model.Chain
 import io.novafoundation.nova.runtime.multiNetwork.chain.model.ChainId
 import io.novafoundation.nova.runtime.multiNetwork.connection.autobalance.strategy.AutoBalanceStrategyProvider
-import jp.co.soramitsu.fearless_utils.wsrpc.state.SocketStateMachine
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.launchIn
@@ -21,7 +19,7 @@ class NodeAutobalancer(
 
     fun balancingNodeFlow(
         chainId: ChainId,
-        socketStateFlow: Flow<SocketStateMachine.State>,
+        changeConnectionEventFlow: Flow<Unit>,
         availableNodesFlow: Flow<List<Chain.Node>>,
         scope: CoroutineScope,
     ): Flow<Chain.Node> {
@@ -35,14 +33,11 @@ class NodeAutobalancer(
         }
             .onEach { (strategy, nodes) -> result.emit(strategy.initialNode(nodes)) }
             .flatMapLatest { (strategy, nodes) ->
-                socketStateFlow
-                    .filter { it.needsAutobalance() }
+                changeConnectionEventFlow
                     .map { strategy.nextNode(result.first(), nodes) }
             }.onEach(result::emit)
             .launchIn(scope)
 
         return result
     }
-
-    private fun SocketStateMachine.State.needsAutobalance() = this is SocketStateMachine.State.WaitingForReconnect && attempt > 3
 }
