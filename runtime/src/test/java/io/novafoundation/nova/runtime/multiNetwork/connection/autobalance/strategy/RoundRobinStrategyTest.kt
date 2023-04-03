128203292772698c918943373a0e51f551ccf8f6
antonijzelinskij <antonijzelinskij@gmail.com>
2023-04-03 15:33:05 +0500

    Update RoundRobinStrategyTest.kt

--- runtime/src/test/java/io/novafoundation/nova/runtime/multiNetwork/connection/autobalance/strategy/RoundRobinStrategyTest.kt
+++ runtime/src/test/java/io/novafoundation/nova/runtime/multiNetwork/connection/autobalance/strategy/RoundRobinStrategyTest.kt
@@ -1,7 +1,6 @@
 package io.novafoundation.nova.runtime.multiNetwork.connection.autobalance.strategy
 
 import io.novafoundation.nova.runtime.multiNetwork.chain.model.Chain
-import java.lang.IndexOutOfBoundsException
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -9,50 +8,28 @@ class RoundRobinStrategyTest {
 
     private val strategy = RoundRobinStrategy()
 
-    @Test(expected = IndexOutOfBoundsException::class)
-    fun `should select first node if current is not present in available`() = runTest(
-        current = createFakeNode("3"),
-        all = listOf(
-            createFakeNode("1"),
-            createFakeNode("2")
-        ),
-        expected = null
+    private val nodes = listOf(
+        createFakeNode("1"),
+        createFakeNode("2"),
+        createFakeNode("3")
     )
 
     @Test
-    fun `should select first node after last`() = runTest(
-        current = createFakeNode("3"),
-        all = listOf(
-            createFakeNode("1"),
-            createFakeNode("2"),
-            createFakeNode("3")
-        ),
-        expected = createFakeNode("1")
-    )
+    fun `collections should have the same sequence`() {
+        val iterator = strategy.generateNodeSequence(nodes)
+            .iterator()
+
+        nodes.forEach { assertEquals(it, iterator.next()) }
+    }
 
     @Test
-    fun `should select next node if not last`() = runTest(
-        current = createFakeNode("2"),
-        all = listOf(
-            createFakeNode("1"),
-            createFakeNode("2"),
-            createFakeNode("3")
-        ),
-        expected = createFakeNode("3")
-    )
+    fun `sequence should be looped`() {
+        val iterator = strategy.generateNodeSequence(nodes)
+            .iterator()
 
-    private fun runTest(
-        current: Chain.Node,
-        all: List<Chain.Node>,
-        expected: Chain.Node?,
-    ) {
-        val nodeSequence = strategy.generateNodeSequence(all)
-        val indexOfCurrent = all.indexOf(current)
-        if (indexOfCurrent == -1) throw IndexOutOfBoundsException()
+        repeat(nodes.size) { iterator.next() }
 
-        val nextItem = nodeSequence.elementAt(indexOfCurrent + 1)
-
-        assertEquals(expected, nextItem)
+        assertEquals(nodes.first(), iterator.next())
     }
 
     private fun createFakeNode(id: String) = Chain.Node(unformattedUrl = id, name = id, chainId = "test", orderId = 0)
