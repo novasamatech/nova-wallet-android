83e4f8e58e9cbc35080d11af33c25e020b612d8f
Valentun <vallentunn@gmail.com>
2023-06-13 15:38:30 +0700

    Revert "Return custom staking periods (#941)"
    This reverts commit 54eaadc6d9c29f1e75805406aa53ffb1f0381694.

--- feature-staking-impl/src/main/java/io/novafoundation/nova/feature_staking_impl/data/repository/StakingRewardsRepository.kt
+++ feature-staking-impl/src/main/java/io/novafoundation/nova/feature_staking_impl/data/repository/StakingRewardsRepository.kt
@@ -16,7 +16,7 @@ class StakingRewardsRepository(
     }
 
     suspend fun sync(accountAddress: String, chain: Chain, chainAsset: Chain.Asset, rewardPeriod: RewardPeriod) {
-        stakingRewardsDataSource.sync(accountAddress, chain, chainAsset, rewardPeriod)
+        stakingRewardsDataSource.sync(accountAddress, chain, chainAsset) // TODO: provide reward period
     }
 
     suspend fun clearRewards() {
