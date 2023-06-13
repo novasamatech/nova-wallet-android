b1ac189c58577421858b8bbd44ce353966d04652
antonijzelinskij <antonijzelinskij@gmail.com>
2023-06-13 13:12:06 +0500

    Update OperationModel.kt

--- feature-assets/src/main/java/io/novafoundation/nova/feature_assets/presentation/model/OperationModel.kt
+++ feature-assets/src/main/java/io/novafoundation/nova/feature_assets/presentation/model/OperationModel.kt
@@ -1,15 +1,17 @@
 package io.novafoundation.nova.feature_assets.presentation.model
 
+import android.text.TextUtils
 import androidx.annotation.ColorRes
 import io.novafoundation.nova.common.utils.images.Icon
 
 class OperationModel(
     val id: String,
     val amount: String,
-    val fiatWithTime: String?,
+    val amountDetails: String?,
     @ColorRes val amountColorRes: Int,
     val header: String,
     val statusAppearance: OperationStatusAppearance,
     val operationIcon: Icon,
-    val subHeader: String
+    val subHeader: String,
+    val subHeaderEllipsize: TextUtils.TruncateAt
 )
