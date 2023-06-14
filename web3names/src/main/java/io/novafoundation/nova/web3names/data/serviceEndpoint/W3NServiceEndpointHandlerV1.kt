3d3c4ac5c3554716bc2a853cd2b65c4dad483ffc
antonijzelinskij <107959809+antonijzelinskij@users.noreply.github.com>
2023-06-14 12:09:26 +0500

    Feature/w3n v2 (#958)
    * W3N kilt V2
    
    * Update W3NServiceEndpointHandlerV2.kt
    
    * Fixing tests
    
    Fixed test for V1
    Added test for V2
    Fixed verifying in V2
    
    * Removed testnet
    
    * Update ServiceEndpoint.kt
    
    * Update We3NamesIntegrityVerifierTestV2.kt

---
+++ web3names/src/main/java/io/novafoundation/nova/web3names/data/serviceEndpoint/W3NServiceEndpointHandlerV1.kt
@@ -0,0 +1,38 @@
+package io.novafoundation.nova.web3names.data.serviceEndpoint
+
+import com.google.gson.Gson
+import io.ipfs.multibase.Multibase
+import io.novafoundation.nova.common.utils.fromJson
+import io.novafoundation.nova.web3names.data.endpoints.TransferRecipientsApi
+import io.novafoundation.nova.web3names.data.endpoints.model.TransferRecipientDetailsRemoteV1
+import jp.co.soramitsu.fearless_utils.hash.Hasher.blake2b256
+
+private typealias RecipientsByChainV1 = Map<String, List<TransferRecipientDetailsRemoteV1>>
+
+class W3NServiceEndpointHandlerV1(
+    endpoint: ServiceEndpoint,
+    transferRecipientApi: TransferRecipientsApi,
+    private val gson: Gson
+) : W3NServiceEndpointHandler(endpoint, transferRecipientApi) {
+
+    override fun verifyIntegrity(serviceEndpointId: String, serviceEndpointContent: String): Boolean = runCatching {
+        val expectedHash = Multibase.decode(serviceEndpointId)
+
+        val actualHash = serviceEndpointContent.encodeToByteArray().blake2b256()
+
+        expectedHash.contentEquals(actualHash)
+    }.getOrDefault(false)
+
+    override fun parseRecipients(content: String): List<W3NRecepient> {
+        val recipients = gson.fromJson<RecipientsByChainV1>(content)
+        return recipients.flatMap { (chainId, recipients) ->
+            recipients.map { recipient ->
+                W3NRecepient(
+                    chainIdCaip19 = chainId,
+                    account = recipient.account,
+                    description = recipient.description
+                )
+            }
+        }
+    }
+}
