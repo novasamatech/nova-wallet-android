28577476f857a11f7f25e363c9b12b6f3591e0fd
antonijzelinskij <antonijzelinskij@gmail.com>
2023-06-29 13:14:14 +0500

    Fixed yield boost binding

--- feature-staking-impl/src/main/java/io/novafoundation/nova/feature_staking_impl/domain/parachainStaking/yieldBoost/YieldBoostTask.kt
+++ feature-staking-impl/src/main/java/io/novafoundation/nova/feature_staking_impl/domain/parachainStaking/yieldBoost/YieldBoostTask.kt
@@ -1,7 +1,9 @@
 package io.novafoundation.nova.feature_staking_impl.domain.parachainStaking.yieldBoost
 
 import io.novafoundation.nova.feature_wallet_api.data.network.blockhain.types.Balance
+import java.math.BigInteger
 import jp.co.soramitsu.fearless_utils.runtime.AccountId
+import kotlin.math.roundToLong
 import kotlin.time.Duration
 import kotlin.time.DurationUnit
 
@@ -9,9 +11,19 @@ class YieldBoostTask(
     val id: String,
     val collator: AccountId,
     val accountMinimum: Balance,
-    val frequency: Duration,
-)
+    val schedule: Schedule,
+) {
 
-fun YieldBoostTask.frequencyInDays() = frequency.toInt(DurationUnit.DAYS).coerceAtLeast(1)
+    sealed interface Schedule {
+        object Unknown : Schedule
+
+        class Recurring(val frequency: Duration) : Schedule
+    }
+}
+
+fun YieldBoostTask.frequencyInDays() = when (schedule) {
+    is YieldBoostTask.Schedule.Recurring -> schedule.frequency.toInt(DurationUnit.DAYS).coerceAtLeast(1)
+    YieldBoostTask.Schedule.Unknown -> null
+}
 
 fun List<YieldBoostTask>.findByCollator(collatorId: AccountId): YieldBoostTask? = find { it.collator.contentEquals(collatorId) }
