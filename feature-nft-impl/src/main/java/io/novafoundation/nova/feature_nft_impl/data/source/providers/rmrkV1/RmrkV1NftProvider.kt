762f053a96120b64d2e6fa55455cef9f7c809da0
antonijzelinskij <107959809+antonijzelinskij@users.noreply.github.com>
2023-07-10 12:59:14 +0500

    Removed rmrk v1 sync (#998)
    * Removed rmrk v1 sync
    
    * Remove collection sync for RMRK V1
    
    * Run ktlint

--- feature-nft-impl/src/main/java/io/novafoundation/nova/feature_nft_impl/data/source/providers/rmrkV1/RmrkV1NftProvider.kt
+++ feature-nft-impl/src/main/java/io/novafoundation/nova/feature_nft_impl/data/source/providers/rmrkV1/RmrkV1NftProvider.kt
@@ -2,22 +2,17 @@ package io.novafoundation.nova.feature_nft_impl.data.source.providers.rmrkV1
 
 import io.novafoundation.nova.common.utils.flowOf
 import io.novafoundation.nova.core_db.dao.NftDao
-import io.novafoundation.nova.core_db.model.NftLocal
 import io.novafoundation.nova.feature_account_api.domain.interfaces.AccountRepository
 import io.novafoundation.nova.feature_account_api.domain.model.MetaAccount
 import io.novafoundation.nova.feature_account_api.domain.model.accountIdIn
-import io.novafoundation.nova.feature_account_api.domain.model.addressIn
 import io.novafoundation.nova.feature_nft_api.data.model.Nft
 import io.novafoundation.nova.feature_nft_api.data.model.NftDetails
 import io.novafoundation.nova.feature_nft_impl.data.mappers.nftIssuance
 import io.novafoundation.nova.feature_nft_impl.data.mappers.nftPrice
-import io.novafoundation.nova.feature_nft_impl.data.network.distributed.FileStorageAdapter.adoptFileStorageLinkToHttps
 import io.novafoundation.nova.feature_nft_impl.data.source.NftProvider
 import io.novafoundation.nova.feature_nft_impl.data.source.providers.rmrkV1.network.RmrkV1Api
-import io.novafoundation.nova.runtime.ext.accountIdOf
 import io.novafoundation.nova.runtime.multiNetwork.ChainRegistry
 import io.novafoundation.nova.runtime.multiNetwork.chain.model.Chain
-import io.novafoundation.nova.runtime.multiNetwork.chain.model.ChainId
 import kotlinx.coroutines.flow.Flow
 
 class RmrkV1NftProvider(
@@ -28,52 +23,11 @@ class RmrkV1NftProvider(
 ) : NftProvider {
 
     override suspend fun initialNftsSync(chain: Chain, metaAccount: MetaAccount, forceOverwrite: Boolean) {
-        val address = metaAccount.addressIn(chain) ?: return
-        val nfts = api.getNfts(address)
-
-        val toSave = nfts.map {
-            NftLocal(
-                identifier = identifier(chain.id, it.id),
-                metaId = metaAccount.id,
-                chainId = chain.id,
-                collectionId = it.collectionId,
-                instanceId = it.instance,
-                metadata = it.metadata.encodeToByteArray(),
-                name = it.name,
-                label = null,
-                price = it.price,
-                type = NftLocal.Type.RMRK1,
-                issuanceMyEdition = it.edition,
-
-                // to load at full sync
-                media = null,
-                issuanceTotal = null,
-
-                wholeDetailsLoaded = false
-            )
-        }
-
-        nftDao.insertNftsDiff(NftLocal.Type.RMRK1, metaAccount.id, toSave, forceOverwrite)
+        throw UnsupportedOperationException("RmrkV1 doesn't supported")
     }
 
     override suspend fun nftFullSync(nft: Nft) {
-        val type = nft.type
-        require(type is Nft.Type.Rmrk1)
-
-        val collection = api.getCollection(type.collectionId)
-
-        val metadata = nft.metadataRaw?.let {
-            api.getIpfsMetadata(it.decodeToString().adoptFileStorageLinkToHttps())
-        }
-
-        nftDao.updateNft(nft.identifier) { local ->
-            local.copy(
-                media = metadata?.image?.adoptFileStorageLinkToHttps(),
-                label = metadata?.description,
-                issuanceTotal = collection.first().max,
-                wholeDetailsLoaded = true
-            )
-        }
+        throw UnsupportedOperationException("RmrkV1 doesn't supported")
     }
 
     override fun nftDetailsFlow(nftIdentifier: String): Flow<NftDetails> {
@@ -85,32 +39,18 @@ class RmrkV1NftProvider(
             val chain = chainRegistry.getChain(nftLocal.chainId)
             val metaAccount = accountRepository.getMetaAccount(nftLocal.metaId)
 
-            val collection = api.getCollection(nftLocal.collectionId).first()
-
-            val metadata = collection.metadata?.let {
-                api.getIpfsMetadata(it.adoptFileStorageLinkToHttps())
-            }
-
             NftDetails(
                 identifier = nftLocal.identifier,
                 chain = chain,
                 owner = metaAccount.accountIdIn(chain)!!,
-                creator = chain.accountIdOf(collection.issuer),
+                creator = null,
                 media = nftLocal.media,
                 name = nftLocal.name!!,
                 description = nftLocal.label,
                 issuance = nftIssuance(nftLocal),
                 price = nftPrice(nftLocal),
-                collection = NftDetails.Collection(
-                    id = nftLocal.collectionId,
-                    name = collection.name,
-                    media = metadata?.image?.adoptFileStorageLinkToHttps()
-                )
+                collection = null
             )
         }
-    }
-
-    private fun identifier(chainId: ChainId, id: String): String {
-        return "$chainId-$id"
     }
 }
