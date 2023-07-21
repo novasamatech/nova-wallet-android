ce60c17e94011f2a559c78757845442983b8cb68
antonijzelinskij <107959809+antonijzelinskij@users.noreply.github.com>
2023-07-21 13:45:04 +0500

    Fixed reward periods (#1028)
    * Fixed reward periods
    
    * Fixed imports
    
    * Commented code

--- feature-staking-impl/src/main/java/io/novafoundation/nova/feature_staking_impl/data/repository/datasource/SubqueryStakingRewardsDataSource.kt
+++ feature-staking-impl/src/main/java/io/novafoundation/nova/feature_staking_impl/data/repository/datasource/SubqueryStakingRewardsDataSource.kt
@@ -1,5 +1,7 @@
 package io.novafoundation.nova.feature_staking_impl.data.repository.datasource
 
+import io.novafoundation.nova.common.utils.atTheBeginningOfTheDay
+import io.novafoundation.nova.common.utils.atTheEndOfTheDay
 import io.novafoundation.nova.common.utils.timestamp
 import io.novafoundation.nova.core_db.dao.StakingTotalRewardDao
 import io.novafoundation.nova.core_db.model.TotalRewardLocal
@@ -29,8 +31,10 @@ class SubqueryStakingRewardsDataSource(
 
     override suspend fun sync(accountAddress: String, chain: Chain, chainAsset: Chain.Asset, rewardPeriod: RewardPeriod) {
         val stakingExternalApi = chain.stakingExternalApi() ?: return
-        val start = rewardPeriod.start?.timestamp()
-        val end = rewardPeriod.end?.timestamp()
+        val start = rewardPeriod.start?.atTheBeginningOfTheDay() // Using atTheBeginningOfTheDay() to avoid invalid data
+            ?.timestamp()
+        val end = rewardPeriod.end?.atTheEndOfTheDay() // Using atTheEndOfTheDay() since the end of the day is fully included in the period
+            ?.timestamp()
 
         val response = stakingApi.getRewardsByPeriod(
             url = stakingExternalApi.url,
