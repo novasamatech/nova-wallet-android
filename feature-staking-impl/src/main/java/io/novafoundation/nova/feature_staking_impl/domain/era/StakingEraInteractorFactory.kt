ebc1389678a234c7b6967a4ffae318e0e4d22ec4
antonijzelinskij <antonijzelinskij@gmail.com>
2023-08-03 03:27:25 +0500

    Update StakingEraInteractorFactory.kt

--- feature-staking-impl/src/main/java/io/novafoundation/nova/feature_staking_impl/domain/era/StakingEraInteractorFactory.kt
+++ feature-staking-impl/src/main/java/io/novafoundation/nova/feature_staking_impl/domain/era/StakingEraInteractorFactory.kt
@@ -1,7 +1,6 @@
 package io.novafoundation.nova.feature_staking_impl.domain.era
 
 import io.novafoundation.nova.feature_staking_impl.data.parachainStaking.RoundDurationEstimator
-import io.novafoundation.nova.feature_staking_impl.data.parachainStaking.repository.CurrentRoundRepository
 import io.novafoundation.nova.feature_staking_impl.domain.StakingInteractor
 import io.novafoundation.nova.runtime.ext.StakingTypeGroup
 import io.novafoundation.nova.runtime.ext.group
