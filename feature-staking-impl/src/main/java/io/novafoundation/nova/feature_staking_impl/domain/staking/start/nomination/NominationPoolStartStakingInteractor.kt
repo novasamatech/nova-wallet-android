284eff46b89b15d507543e75608c73b9386637f6
antonijzelinskij <antonijzelinskij@gmail.com>
2023-08-03 02:24:13 +0500

    Update NominationPoolStartStakingInteractor.kt

--- feature-staking-impl/src/main/java/io/novafoundation/nova/feature_staking_impl/domain/staking/start/nomination/NominationPoolStartStakingInteractor.kt
+++ feature-staking-impl/src/main/java/io/novafoundation/nova/feature_staking_impl/domain/staking/start/nomination/NominationPoolStartStakingInteractor.kt
@@ -8,6 +8,7 @@ import io.novafoundation.nova.feature_wallet_api.domain.model.Asset
 import io.novafoundation.nova.runtime.multiNetwork.chain.model.Chain
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
 
 class NominationPoolStartStakingInteractor(
     private val nominationPoolGlobalsRepository: NominationPoolGlobalsRepository
