0d9361883ef9d6e95c2229196ef35079e73c83fc
valentunn <70131744+valentunn@users.noreply.github.com>
2023-08-16 18:22:10 +0700

    Warn users when sending tokens to system account (#1063)
    * Warn users when sending tokens to system account
    
    * Code style
    
    * Update strings

---
+++ feature-account-api/src/main/java/io/novafoundation/nova/feature_account_api/domain/account/system/PrefixSystemAccountMatcher.kt
@@ -0,0 +1,13 @@
+package io.novafoundation.nova.feature_account_api.domain.account.system
+
+import io.novafoundation.nova.common.utils.startsWith
+import jp.co.soramitsu.fearless_utils.runtime.AccountId
+
+class PrefixSystemAccountMatcher(prefix: String) : SystemAccountMatcher {
+
+    private val prefixBytes = prefix.encodeToByteArray()
+
+    override fun isSystemAccount(accountId: AccountId): Boolean {
+        return accountId.startsWith(prefixBytes)
+    }
+}
