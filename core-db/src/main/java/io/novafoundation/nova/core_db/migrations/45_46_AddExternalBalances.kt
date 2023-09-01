de2efd88fa089ce63c18ffc858444c90580e78bb
Valentun <vallentunn@gmail.com>
2023-09-01 14:56:46 +0700

    Code style

--- core-db/src/main/java/io/novafoundation/nova/core_db/migrations/45_46_AddExternalBalances.kt
+++ core-db/src/main/java/io/novafoundation/nova/core_db/migrations/45_46_AddExternalBalances.kt
@@ -2,8 +2,6 @@ package io.novafoundation.nova.core_db.migrations
 
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
-import io.novafoundation.nova.core_db.converters.ExternalBalanceTypeConverters
-import io.novafoundation.nova.core_db.model.ExternalBalanceLocal
 
 val AddExternalBalances_45_46 = object : Migration(45, 46) {
 
@@ -21,24 +19,6 @@ val AddExternalBalances_45_46 = object : Migration(45, 46) {
             FOREIGN KEY(`assetId`, `chainId`) REFERENCES `chain_assets`(`id`, `chainId`) ON UPDATE NO ACTION ON DELETE CASCADE,
             FOREIGN KEY(`metaId`) REFERENCES `meta_accounts`(`id`) ON UPDATE NO ACTION ON DELETE CASCADE )
             """
-        )
-
-//        fillExternalBalancesWithContributionsData(database)
-    }
-
-    private fun fillExternalBalancesWithContributionsData(database: SupportSQLiteDatabase) {
-        val converters = ExternalBalanceTypeConverters()
-        val crowdloanId = converters.fromType(ExternalBalanceLocal.Type.CROWDLOAN)
-
-        database.execSQL(
-            """
-            INSERT INTO externalBalances
-            SELECT metaId, chainId, assetId,
-            "$crowdloanId" as type, sourceId as subtype, SUM(amountInPlanks) as amount 
-            FROM contributions
-            WHERE SELECT EXISTS
-            GROUP BY metaId, chainId, assetId, sourceId
-            """.trimIndent()
         )
     }
 }
