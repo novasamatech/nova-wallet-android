c0ac912ea1927614ab035db1dfc7b334e67e903f
antonijzelinskij <antonijzelinskij@gmail.com>
2023-09-11 23:32:40 +0200

    Add validation to pool search

---
+++ feature-staking-impl/src/main/java/io/novafoundation/nova/feature_staking_impl/domain/validations/setup/PoolAvailabilityValidation.kt
@@ -0,0 +1,39 @@
+package io.novafoundation.nova.feature_staking_impl.domain.validations.setup
+
+import io.novafoundation.nova.common.validation.Validation
+import io.novafoundation.nova.common.validation.ValidationStatus
+import io.novafoundation.nova.common.validation.ValidationSystemBuilder
+import io.novafoundation.nova.common.validation.valid
+import io.novafoundation.nova.common.validation.validationError
+import io.novafoundation.nova.feature_staking_impl.data.nominationPools.network.blockhain.models.isOpen
+import io.novafoundation.nova.feature_staking_impl.data.nominationPools.repository.NominationPoolGlobalsRepository
+import io.novafoundation.nova.feature_staking_impl.domain.nominationPools.model.isActive
+import io.novafoundation.nova.feature_staking_impl.domain.nominationPools.selecting.PoolAvailabilityFailure
+import io.novafoundation.nova.feature_staking_impl.domain.nominationPools.selecting.PoolAvailabilityPayload
+
+class PoolAvailabilityValidation(
+    private val nominationPoolGlobalsRepository: NominationPoolGlobalsRepository
+) : Validation<PoolAvailabilityPayload, PoolAvailabilityFailure> {
+
+    override suspend fun validate(value: PoolAvailabilityPayload): ValidationStatus<PoolAvailabilityFailure> {
+        val pool = value.nominationPool
+        return when {
+            !pool.status.isActive || !pool.state.isOpen -> validationError(PoolAvailabilityFailure.PoolIsClosed)
+            isPoolFull(value) -> validationError(PoolAvailabilityFailure.PoolIsFull)
+            else -> valid()
+        }
+    }
+
+    private suspend fun isPoolFull(value: PoolAvailabilityPayload): Boolean {
+        val pool = value.nominationPool
+        val maxPoolMembers = nominationPoolGlobalsRepository.maxPoolMembersPerPool(value.chain.id)
+        return maxPoolMembers != null && maxPoolMembers <= pool.membersCount
+    }
+}
+
+fun ValidationSystemBuilder<PoolAvailabilityPayload, PoolAvailabilityFailure>.poolAvailable(
+    nominationPoolGlobalsRepository: NominationPoolGlobalsRepository
+) {
+    val validation = PoolAvailabilityValidation(nominationPoolGlobalsRepository = nominationPoolGlobalsRepository)
+    validate(validation)
+}
