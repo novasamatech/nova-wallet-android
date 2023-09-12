a39ac730e445f912e43f77fb8179b59af70d3b02
Valentun <vallentunn@gmail.com>
2023-09-12 11:32:32 +0700

    Reuse existing string

--- feature-assets/src/main/java/io/novafoundation/nova/feature_assets/presentation/transaction/history/mixin/OperationMappers.kt
+++ feature-assets/src/main/java/io/novafoundation/nova/feature_assets/presentation/transaction/history/mixin/OperationMappers.kt
@@ -185,7 +185,7 @@ fun mapOperationToOperationModel(
         when (val operationType = type) {
             is Operation.Type.Reward -> {
                 val headerResId = if (operationType.isReward) R.string.staking_reward else R.string.staking_slash
-                val subtitleRes = if (operationType.kind is RewardKind.Direct) R.string.tabbar_staking_title else R.string.transactions_pool_staking_subtitle
+                val subtitleRes = if (operationType.kind is RewardKind.Direct) R.string.tabbar_staking_title else R.string.setup_staking_type_pool_staking
                 OperationModel(
                     id = id,
                     amount = formatAmount(chainAsset, operationType),
