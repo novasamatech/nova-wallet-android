0cb151ecff774dda6c1d146026915c92899a7369
valentun <vallentunn@gmail.com>
2023-09-28 17:13:37 +0300

    Improve swap service tests

--- app/src/androidTest/java/io/novafoundation/nova/SwapServiceIntegrationTest.kt
+++ app/src/androidTest/java/io/novafoundation/nova/SwapServiceIntegrationTest.kt
@@ -4,6 +4,9 @@ import android.util.Log
 import io.novafoundation.nova.common.di.FeatureUtils
 import io.novafoundation.nova.feature_swap_api.di.SwapFeatureApi
 import io.novafoundation.nova.feature_swap_impl.di.SwapFeatureComponent
+import io.novafoundation.nova.runtime.ext.Geneses
+import io.novafoundation.nova.runtime.multiNetwork.asset
+import io.novafoundation.nova.runtime.multiNetwork.chain.model.Chain
 import io.novafoundation.nova.runtime.multiNetwork.chainWithAsset
 import org.junit.Test
 
@@ -11,15 +14,27 @@ class SwapServiceIntegrationTest : BaseIntegrationTest() {
 
     private val swapApi = FeatureUtils.getFeature<SwapFeatureComponent>(context, SwapFeatureApi::class.java)
 
+    private val swapService = swapApi.swapService
+
     @Test
     fun shouldRetrieveAvailableDirections() = runTest {
-        val allAvailableAssetIdsToSwap = swapApi.swapService.assetsAvailableForSwap(this)
+        val allAvailableAssetIdsToSwap = swapService.assetsAvailableForSwap(this)
         val allAvailableChainAssetsToSwap = allAvailableAssetIdsToSwap.map {
             val (chain, asset) = chainRegistry.chainWithAsset(it.chainId, it.assetId)
 
-            "${chain.name}.${asset.symbol}"
+            "${chain.name}::${asset.symbol}"
         }
 
-        Log.d("SwapServiceIntegrationTest", allAvailableChainAssetsToSwap.joinToString())
+        Log.d("SwapServiceIntegrationTest", "All available tokens: ${allAvailableChainAssetsToSwap.joinToString()}")
+
+        val wndOnWestmint = chainRegistry.asset(Chain.Geneses.WESTMINT, 0)
+        val directionsForWnd = swapService.availableSwapDirectionsFor(wndOnWestmint, this)
+        val directionsForWndFormatted = directionsForWnd.map { otherId ->
+            val asset = chainRegistry.asset(otherId)
+
+            "${wndOnWestmint.symbol} - ${asset.symbol}"
+        }
+
+        Log.d("SwapServiceIntegrationTest", "Available directions for ${wndOnWestmint.symbol}: ${directionsForWndFormatted.joinToString()}")
     }
 }
