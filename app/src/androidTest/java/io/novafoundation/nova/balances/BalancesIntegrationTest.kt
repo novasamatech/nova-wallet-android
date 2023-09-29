5deb083613c9bd4c41a2f8e662c55a7f0e684ae6
valentun <vallentunn@gmail.com>
2023-09-29 15:41:03 +0300

    Fix - asset hub signing

--- app/src/androidTest/java/io/novafoundation/nova/balances/BalancesIntegrationTest.kt
+++ app/src/androidTest/java/io/novafoundation/nova/balances/BalancesIntegrationTest.kt
@@ -8,9 +8,12 @@ import io.novafoundation.nova.common.data.network.runtime.binding.bindAccountInf
 import io.novafoundation.nova.common.di.FeatureUtils
 import io.novafoundation.nova.common.utils.fromJson
 import io.novafoundation.nova.common.utils.system
+import io.novafoundation.nova.feature_account_api.di.AccountFeatureApi
+import io.novafoundation.nova.feature_account_impl.di.AccountFeatureComponent
 import io.novafoundation.nova.runtime.BuildConfig.TEST_CHAINS_URL
 import io.novafoundation.nova.runtime.di.RuntimeApi
 import io.novafoundation.nova.runtime.di.RuntimeComponent
+import io.novafoundation.nova.runtime.extrinsic.systemRemark
 import io.novafoundation.nova.runtime.multiNetwork.chain.model.Chain
 import io.novafoundation.nova.runtime.multiNetwork.connection.ChainConnection
 import io.novafoundation.nova.runtime.multiNetwork.getSocket
@@ -18,21 +21,20 @@ import jp.co.soramitsu.fearless_utils.extensions.fromHex
 import jp.co.soramitsu.fearless_utils.runtime.metadata.storage
 import jp.co.soramitsu.fearless_utils.runtime.metadata.storageKey
 import jp.co.soramitsu.fearless_utils.wsrpc.networkStateFlow
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withTimeout
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import java.math.BigInteger
 import java.math.BigInteger.ZERO
 import java.net.URL
-import kotlin.time.ExperimentalTime
-import kotlin.time.seconds
+import kotlin.time.Duration.Companion.seconds
 
 
 @RunWith(Parameterized::class)
@@ -66,28 +68,45 @@ class BalancesIntegrationTest(
         RuntimeApi::class.java
     )
 
+    private val accountApi = FeatureUtils.getFeature<AccountFeatureComponent>(
+        ApplicationProvider.getApplicationContext<Context>(),
+        AccountFeatureApi::class.java
+    )
+
     private val chainRegistry = runtimeApi.chainRegistry()
     private val externalRequirementFlow = runtimeApi.externalRequirementFlow()
 
     private val remoteStorage = runtimeApi.remoteStorageSource()
 
+    private val extrinsicService = accountApi.extrinsicService()
+
+    @Before
+    fun before() = runBlocking {
+        externalRequirementFlow.emit(ChainConnection.ExternalRequirement.ALLOWED)
+    }
+
     @Test
     fun testBalancesLoading() = runBlocking(Dispatchers.Default) {
-        externalRequirementFlow.emit(ChainConnection.ExternalRequirement.ALLOWED)
+        val chains = chainRegistry.getChain(testChainId)
 
-        val chains = chainRegistry.currentChains.first()
+        val freeBalance = testBalancesInChainAsync(chains, testAccount)?.data?.free ?: error("Balance was null")
 
-        val errorMessage = chains.find { it.id == testChainId }?.let { it -> testBalancesInChainAsync(it, testAccount) }
-
-        val freeBalance = errorMessage?.map { it?.data?.free }?.getOrNull() ?: throw errorMessage?.exceptionOrNull()!!
         assertTrue("Free balance: $freeBalance is less than $maxAmount", maxAmount > freeBalance)
         assertTrue("Free balance: $freeBalance is greater than 0", ZERO < freeBalance)
     }
 
-    @OptIn(ExperimentalTime::class)
-    private suspend fun CoroutineScope.testBalancesInChainAsync(chain: Chain, currentAccount: String): Result<AccountInfo?> {
+    @Test
+    fun testFeeLoading() = runBlocking(Dispatchers.Default) {
+        val chains = chainRegistry.getChain(testChainId)
+
+        testFeeLoadingAsync(chains)
+
+        Unit
+    }
+
+    private suspend fun testBalancesInChainAsync(chain: Chain, currentAccount: String): AccountInfo? {
         return coroutineScope {
-            runCatching {
+            try {
                 withTimeout(80.seconds) {
                     remoteStorage.query(
                         chainId = chain.id,
@@ -95,8 +114,20 @@ class BalancesIntegrationTest(
                         binding = { scale, runtime -> scale?.let { bindAccountInfo(scale, runtime) } }
                     )
                 }
-            }.recover {
-                throw Exception("Socket state: ${chainRegistry.getSocket(chain.id).networkStateFlow().first()}, error: ${it.message}", it)
+            } catch(e: Exception) {
+                throw Exception("Socket state: ${chainRegistry.getSocket(chain.id).networkStateFlow().first()}, error: ${e.message}", e)
+            }
+        }
+    }
+
+    private suspend fun testFeeLoadingAsync(chain: Chain): Result<*> {
+        return coroutineScope {
+            runCatching {
+                withTimeout(80.seconds) {
+                    extrinsicService.estimateFee(chain) {
+                        systemRemark(byteArrayOf(0))
+                    }
+                }
             }
         }
     }
