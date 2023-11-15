9e104c7298ec80aea5e8482d4583c26b8367778c
valentunn <70131744+valentunn@users.noreply.github.com>
2023-11-15 23:26:01 +0700

    Fix - inactive pool staking after pool has changed the validators (#1231)

--- feature-staking-impl/src/main/java/io/novafoundation/nova/feature_staking_impl/domain/nominationPools/common/poolState/PoolState.kt
+++ feature-staking-impl/src/main/java/io/novafoundation/nova/feature_staking_impl/domain/nominationPools/common/poolState/PoolState.kt
@@ -11,10 +11,24 @@ fun AccountIdMap<Exposure>.isPoolStaking(poolStash: AccountId, poolNominations: 
     // we still mark it as Inactive to warn user preemptively
     if (poolNominations == null) return false
 
+    // fast path - we don't need to traverse the entire exposure map if pool is staking with some of its current validators
+    if (isPoolStakingWithCurrentNominations(poolStash, poolNominations)) return true
+
+    // slow path - despite pool is not staking with its current nomination it might still stake with a validator if it changed nominations recently
+    return isPoolStakingWithAnyValidator(poolStash)
+}
+
+private fun AccountIdMap<Exposure>.isPoolStakingWithCurrentNominations(poolStash: AccountId, poolNominations: Nominations): Boolean {
     return poolNominations.targets.any { validator ->
         val accountIdHex = validator.toHexString()
         val exposure = get(accountIdHex) ?: return@any false
 
+        exposure.others.any { it.who.contentEquals(poolStash) }
+    }
+}
+
+private fun AccountIdMap<Exposure>.isPoolStakingWithAnyValidator(poolStash: AccountId): Boolean {
+    return any { (_, exposure) ->
         exposure.others.any { it.who.contentEquals(poolStash) }
     }
 }
