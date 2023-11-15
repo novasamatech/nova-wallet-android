7c767f4433c5116d3f2127a07a21a4940589dd46
valentunn <70131744+valentunn@users.noreply.github.com>
2023-11-15 23:25:50 +0700

    Display total requested amount for batch calls (#1230)
    * Display total requested amount for batch calls
    
    * Code style

--- feature-governance-impl/src/main/java/io/novafoundation/nova/feature_governance_impl/domain/referendum/details/call/treasury/TreasuryApproveProposalAdapter.kt
+++ feature-governance-impl/src/main/java/io/novafoundation/nova/feature_governance_impl/domain/referendum/details/call/treasury/TreasuryApproveProposalAdapter.kt
@@ -3,26 +3,24 @@ package io.novafoundation.nova.feature_governance_impl.domain.referendum.details
 import io.novafoundation.nova.common.data.network.runtime.binding.bindNumber
 import io.novafoundation.nova.common.utils.Modules
 import io.novafoundation.nova.common.utils.instanceOf
-import io.novafoundation.nova.feature_governance_api.data.network.blockhain.model.PreImage
 import io.novafoundation.nova.feature_governance_api.data.network.blockhain.model.TreasuryProposal
 import io.novafoundation.nova.feature_governance_api.data.repository.TreasuryRepository
 import io.novafoundation.nova.feature_governance_api.domain.referendum.details.ReferendumCall
-import io.novafoundation.nova.feature_governance_impl.domain.referendum.details.call.ReferendumCallParser
-import io.novafoundation.nova.runtime.multiNetwork.chain.model.ChainId
+import io.novafoundation.nova.feature_governance_impl.domain.referendum.details.call.ReferendumCallAdapter
+import io.novafoundation.nova.feature_governance_impl.domain.referendum.details.call.ReferendumCallParseContext
+import jp.co.soramitsu.fearless_utils.runtime.definitions.types.generics.GenericCall
 
-class TreasuryApproveProposalParser(
+class TreasuryApproveProposalAdapter(
     private val treasuryRepository: TreasuryRepository
-) : ReferendumCallParser {
+) : ReferendumCallAdapter {
 
-    override suspend fun parse(preImage: PreImage, chainId: ChainId): ReferendumCall? = runCatching {
-        val call = preImage.call
-
+    override suspend fun fromCall(call: GenericCall.Instance, context: ReferendumCallParseContext): ReferendumCall? {
         if (!call.instanceOf(Modules.TREASURY, "approve_proposal")) return null
 
         val id = bindNumber(call.arguments["proposal_id"])
 
-        val proposal = treasuryRepository.getTreasuryProposal(chainId, TreasuryProposal.Id(id)) ?: return null
+        val proposal = treasuryRepository.getTreasuryProposal(context.chainId, TreasuryProposal.Id(id)) ?: return null
 
-        ReferendumCall.TreasuryRequest(amount = proposal.amount, beneficiary = proposal.beneficiary)
-    }.getOrNull()
+        return ReferendumCall.TreasuryRequest(amount = proposal.amount, beneficiary = proposal.beneficiary)
+    }
 }
