d01325c6f49ad7b479addfbabb1f9d28ffed2392
antonijzelinskij <antonijzelinskij@gmail.com>
2023-11-29 16:26:00 +0100

    Add description to navigation update

--- app/src/main/java/io/novafoundation/nova/app/root/navigation/Navigator.kt
+++ app/src/main/java/io/novafoundation/nova/app/root/navigation/Navigator.kt
@@ -168,6 +168,7 @@ class Navigator(
     override fun openImportAccountScreen(payload: ImportAccountPayload) {
         val currentDestination = navController?.currentDestination ?: return
         val actionId = when (currentDestination.id) {
+            // Wee need the slpash fragment case to close app if we use back navigation in import mnemonic screen
             R.id.splashFragment -> R.id.action_splashFragment_to_import_nav_graph
             else -> R.id.action_import_nav_graph
         }
