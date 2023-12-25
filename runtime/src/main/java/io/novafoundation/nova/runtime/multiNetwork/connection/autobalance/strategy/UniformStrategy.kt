7b92a7b8eee1cc0f78fa4f237beb8dfd0b24b725
Valentun <vallentunn@gmail.com>
2023-12-25 11:52:05 +0300

    Skip nodes with unknown secrets and add dwellir secrets

--- runtime/src/main/java/io/novafoundation/nova/runtime/multiNetwork/connection/autobalance/strategy/UniformStrategy.kt
+++ runtime/src/main/java/io/novafoundation/nova/runtime/multiNetwork/connection/autobalance/strategy/UniformStrategy.kt
@@ -1,11 +1,11 @@
 package io.novafoundation.nova.runtime.multiNetwork.connection.autobalance.strategy
 
 import io.novafoundation.nova.common.utils.cycle
-import io.novafoundation.nova.runtime.multiNetwork.chain.model.Chain
+import io.novafoundation.nova.runtime.multiNetwork.connection.NodeWithSaturatedUrl
 
 class UniformStrategy : AutoBalanceStrategy {
 
-    override fun generateNodeSequence(defaultNodes: List<Chain.Node>): Sequence<Chain.Node> {
+    override fun generateNodeSequence(defaultNodes: List<NodeWithSaturatedUrl>): Sequence<NodeWithSaturatedUrl> {
         return defaultNodes.shuffled().cycle()
     }
 }
