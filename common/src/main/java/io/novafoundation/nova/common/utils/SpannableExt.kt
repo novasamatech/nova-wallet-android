7a4dd2c669a92f9bc77cf1aa0cd2587c8594b921
Valentun <vallentunn@gmail.com>
2024-01-26 13:07:52 +0300

    Fix - crash on start staking on older Android versions

--- common/src/main/java/io/novafoundation/nova/common/utils/SpannableExt.kt
+++ common/src/main/java/io/novafoundation/nova/common/utils/SpannableExt.kt
@@ -1,18 +1,23 @@
 package io.novafoundation.nova.common.utils
 
+import android.annotation.TargetApi
 import android.content.Context
 import android.graphics.Typeface
 import android.graphics.drawable.Drawable
+import android.os.Build
 import android.text.Spannable
 import android.text.SpannableStringBuilder
 import android.text.SpannedString
 import android.text.TextPaint
+import android.text.style.CharacterStyle
 import android.text.style.ClickableSpan
 import android.text.style.ForegroundColorSpan
 import android.text.style.ImageSpan
+import android.text.style.MetricAffectingSpan
 import android.text.style.TypefaceSpan
 import android.view.View
 import androidx.annotation.FontRes
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.text.toSpannable
 import io.novafoundation.nova.common.resources.ResourceManager
 import io.novafoundation.nova.common.utils.formatting.spannable.SpannableFormatter
@@ -64,12 +69,39 @@ fun colorSpan(color: Int) = ForegroundColorSpan(color)
 
 fun fontSpan(resourceManager: ResourceManager, @FontRes fontRes: Int) = fontSpan(resourceManager.getFont(fontRes))
 
-fun fontSpan(context: Context, @FontRes fontRes: Int) = fontSpan(context.resources.getFont(fontRes))
+fun fontSpan(context: Context, @FontRes fontRes: Int) = fontSpan(ResourcesCompat.getFont(context, fontRes))
 
-fun fontSpan(typeface: Typeface) = TypefaceSpan(typeface)
+fun fontSpan(typeface: Typeface?): CharacterStyle {
+    return when {
+        typeface == null -> NoOpSpan()
+
+        Build.VERSION.SDK_INT>= Build.VERSION_CODES.P -> typefaceSpanCompatV28(typeface)
+
+        else -> CustomTypefaceSpan(typeface)
+    }
+}
 
 fun drawableSpan(drawable: Drawable) = ImageSpan(drawable)
 
 fun CharSequence.formatAsSpannable(vararg args: Any): SpannedString {
     return SpannableFormatter.format(this, *args)
+}
+
+@TargetApi(Build.VERSION_CODES.P)
+private fun typefaceSpanCompatV28(typeface: Typeface) =
+    TypefaceSpan(typeface)
+
+
+private class CustomTypefaceSpan(private val typeface: Typeface?) : MetricAffectingSpan() {
+    override fun updateDrawState(paint: TextPaint) {
+        paint.typeface = typeface
+    }
+
+    override fun updateMeasureState(paint: TextPaint) {
+        paint.typeface = typeface
+    }
+}
+
+private class NoOpSpan: CharacterStyle() {
+    override fun updateDrawState(tp: TextPaint?) {}
 }
