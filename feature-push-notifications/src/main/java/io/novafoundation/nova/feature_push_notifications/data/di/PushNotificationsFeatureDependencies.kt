0ccfbe743c3b878d533866ac4a8862b330f0abe5
antonijzelinskij <antonijzelinskij@gmail.com>
2024-02-05 12:21:05 +0100

    Update PushNotificationsFeatureDependencies.kt

--- feature-push-notifications/src/main/java/io/novafoundation/nova/feature_push_notifications/data/di/PushNotificationsFeatureDependencies.kt
+++ feature-push-notifications/src/main/java/io/novafoundation/nova/feature_push_notifications/data/di/PushNotificationsFeatureDependencies.kt
@@ -1,5 +1,6 @@
 package io.novafoundation.nova.feature_push_notifications.data.di
 
+import android.content.Context
 import com.google.gson.Gson
 import io.novafoundation.nova.common.data.storage.Preferences
 import io.novafoundation.nova.common.utils.coroutines.RootScope
@@ -11,4 +12,6 @@ interface PushNotificationsFeatureDependencies {
     val rootScope: RootScope
 
     val preferences: Preferences
+
+    val context: Context
 }
