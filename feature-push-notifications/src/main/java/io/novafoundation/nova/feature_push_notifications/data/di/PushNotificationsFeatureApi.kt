86c5bf0c19e7f60f59d9952075fb346d67da3823
antonijzelinskij <antonijzelinskij@gmail.com>
2024-02-15 08:45:19 +0100

    Update PushNotificationsFeatureApi.kt

--- feature-push-notifications/src/main/java/io/novafoundation/nova/feature_push_notifications/data/di/PushNotificationsFeatureApi.kt
+++ feature-push-notifications/src/main/java/io/novafoundation/nova/feature_push_notifications/data/di/PushNotificationsFeatureApi.kt
@@ -1,7 +1,6 @@
 package io.novafoundation.nova.feature_push_notifications.data.di
 
 import io.novafoundation.nova.feature_push_notifications.data.NovaFirebaseMessagingService
-import io.novafoundation.nova.feature_push_notifications.data.data.PushNotificationsService
 import io.novafoundation.nova.feature_push_notifications.data.domain.interactor.PushNotificationsInteractor
 
 interface PushNotificationsFeatureApi {
