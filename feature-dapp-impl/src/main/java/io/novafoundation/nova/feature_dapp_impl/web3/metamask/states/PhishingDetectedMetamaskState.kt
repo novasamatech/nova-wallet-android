59a90e814f11c25197e7d710587c77ae752f3356
valentunn <70131744+valentunn@users.noreply.github.com>
2024-02-21 17:09:19 +0300

    Hotifx/console crashes (#1405)
    * Fix - race condition when setting initial state of send screen in cross-in mode
    
    * Fix - failing error type check when rejecting phishing with Metamask

--- feature-dapp-impl/src/main/java/io/novafoundation/nova/feature_dapp_impl/web3/metamask/states/PhishingDetectedMetamaskState.kt
+++ feature-dapp-impl/src/main/java/io/novafoundation/nova/feature_dapp_impl/web3/metamask/states/PhishingDetectedMetamaskState.kt
@@ -1,6 +1,7 @@
 package io.novafoundation.nova.feature_dapp_impl.web3.metamask.states
 
 import io.novafoundation.nova.feature_dapp_impl.web3.metamask.model.MetamaskChain
+import io.novafoundation.nova.feature_dapp_impl.web3.metamask.transport.MetamaskError
 import io.novafoundation.nova.feature_dapp_impl.web3.metamask.transport.MetamaskTransportRequest
 import io.novafoundation.nova.feature_dapp_impl.web3.states.PhishingDetectedState
 
@@ -8,4 +9,8 @@ class PhishingDetectedMetamaskState(override val chain: MetamaskChain) :
     PhishingDetectedState<MetamaskTransportRequest<*>, MetamaskState>(), MetamaskState {
 
     override val selectedAccountAddress: String? = null
+
+    override fun rejectError(): Throwable {
+        return MetamaskError.Rejected()
+    }
 }
--- feature-dapp-impl/src/main/java/io/novafoundation/nova/feature_dapp_impl/web3/polkadotJs/states/PhishingDetectedPolkadotJsState.kt
+++ feature-dapp-impl/src/main/java/io/novafoundation/nova/feature_dapp_impl/web3/polkadotJs/states/PhishingDetectedPolkadotJsState.kt
@@ -3,4 +3,9 @@ package io.novafoundation.nova.feature_dapp_impl.web3.polkadotJs.states
 import io.novafoundation.nova.feature_dapp_impl.web3.polkadotJs.PolkadotJsTransportRequest
 import io.novafoundation.nova.feature_dapp_impl.web3.states.PhishingDetectedState
 
-class PhishingDetectedPolkadotJsState : PhishingDetectedState<PolkadotJsTransportRequest<*>, PolkadotJsState>(), PolkadotJsState
+class PhishingDetectedPolkadotJsState : PhishingDetectedState<PolkadotJsTransportRequest<*>, PolkadotJsState>(), PolkadotJsState {
+
+    override fun rejectError(): Throwable {
+        return IllegalAccessException("Phishing detected!")
+    }
+}
