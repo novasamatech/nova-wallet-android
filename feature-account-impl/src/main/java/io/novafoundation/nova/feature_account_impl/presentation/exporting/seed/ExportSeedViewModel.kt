73a5ae12d1a7b26b6422b0713324af2ad96d961d
antonijzelinskij <antonijzelinskij@gmail.com>
2024-05-14 12:48:17 +0200

    Change field name for ethereum based chains in seed import

--- feature-account-impl/src/main/java/io/novafoundation/nova/feature_account_impl/presentation/exporting/seed/ExportSeedViewModel.kt
+++ feature-account-impl/src/main/java/io/novafoundation/nova/feature_account_impl/presentation/exporting/seed/ExportSeedViewModel.kt
@@ -1,7 +1,9 @@
 package io.novafoundation.nova.feature_account_impl.presentation.exporting.seed
 
+import io.novafoundation.nova.common.resources.ResourceManager
 import io.novafoundation.nova.common.utils.flowOf
 import io.novafoundation.nova.common.utils.inBackground
+import io.novafoundation.nova.feature_account_impl.R
 import io.novafoundation.nova.feature_account_impl.domain.account.export.seed.ExportPrivateKeyInteractor
 import io.novafoundation.nova.feature_account_impl.presentation.AccountRouter
 import io.novafoundation.nova.feature_account_impl.presentation.account.advancedEncryption.AdvancedEncryptionModePayload
@@ -9,12 +11,21 @@ import io.novafoundation.nova.feature_account_impl.presentation.exporting.Export
 import io.novafoundation.nova.feature_account_impl.presentation.exporting.ExportViewModel
 
 class ExportSeedViewModel(
+    private val resourceManager: ResourceManager,
     private val router: AccountRouter,
     private val interactor: ExportPrivateKeyInteractor,
     private val exportPayload: ExportPayload.ChainAccount,
 ) : ExportViewModel() {
 
-    val seedFlow = flowOf {
+    val secretTypeNameFlow = flowOf {
+        if (interactor.isEthereumBased(exportPayload.chainId)) {
+            resourceManager.getString(R.string.account_private_key)
+        } else {
+            resourceManager.getString(R.string.recovery_raw_seed)
+        }
+    }
+
+    val secretFlow = flowOf {
         if (interactor.isEthereumBased(exportPayload.chainId)) {
             interactor.getEthereumPrivateKey(exportPayload.metaId, exportPayload.chainId)
         } else {
