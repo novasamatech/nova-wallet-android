d2d7e5985375a5008f94da4e1dae302e89e8aaa7
antonijzelinskij <antonijzelinskij@gmail.com>
2024-08-09 10:50:44 +0200

    Update HydrationConversionFeePayment.kt

--- feature-account-impl/src/main/java/io/novafoundation/nova/feature_account_impl/data/fee/types/HydrationConversionFeePayment.kt
+++ feature-account-impl/src/main/java/io/novafoundation/nova/feature_account_impl/data/fee/types/HydrationConversionFeePayment.kt
@@ -1,15 +1,11 @@
 package io.novafoundation.nova.feature_account_impl.data.fee.types
 
-import android.util.Log
-import io.novafoundation.nova.common.utils.LOG_TAG
 import io.novafoundation.nova.feature_account_api.data.fee.FeePayment
 import io.novafoundation.nova.feature_account_api.data.model.Fee
-import io.novafoundation.nova.feature_account_api.data.model.SubstrateFee
 import io.novafoundation.nova.feature_account_api.data.network.hydration.HydraDxAssetIdConverter
 import io.novafoundation.nova.feature_account_api.data.network.hydration.setFeeCurrency
 import io.novafoundation.nova.feature_account_api.data.network.hydration.toOnChainIdOrThrow
 import io.novafoundation.nova.runtime.call.MultiChainRuntimeCallsApi
-import io.novafoundation.nova.runtime.ext.fullId
 import io.novafoundation.nova.runtime.multiNetwork.ChainRegistry
 import io.novafoundation.nova.runtime.multiNetwork.chain.model.Chain
 import io.novasama.substrate_sdk_android.runtime.extrinsic.ExtrinsicBuilder
@@ -31,13 +27,10 @@ internal class HydrationConversionFeePayment(
     }
 
     override suspend fun convertNativeFee(nativeFee: Fee): Fee {
-        val quote = multiChainRuntimeCallsApi.forChain(paymentAsset.chainId).convertNativeFee(nativeFee.amount)
-        requireNotNull(quote) {
-            Log.e(LOG_TAG, "Quote for ${paymentAsset.symbol} fee was null")
+        TODO("Not yet implemented")
+    }
 
-            "Failed to calculate fee in ${paymentAsset.symbol}"
-        }
-
-        return SubstrateFee(amount = quote, submissionOrigin = nativeFee.submissionOrigin, assetId = paymentAsset.fullId)
+    override suspend fun availableCustomFeeAssets(): List<Chain.Asset> {
+        TODO("Not yet implemented")
     }
 }
