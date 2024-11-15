b1091bfc7d6fedb89c8dc3217ad3bd5310ecb6d9
antonijzelinskij <antonijzelinskij@gmail.com>
2024-11-15 17:56:57 +0100

    Delete RealBrowserTabStorage.kt

--- app/src/main/java/io/novafoundation/nova/app/root/data/browser/RealBrowserTabStorage.kt
+++
@@ -1,6 +0,0 @@
-package io.novafoundation.nova.app.root.data.browser
-
-import io.novafoundation.nova.feature_dapp_impl.utils.tabs.models.BrowserTab
-import io.novafoundation.nova.feature_dapp_impl.utils.tabs.models.PageSnapshot
-import io.novafoundation.nova.core_db.model.BrowserTabLocal
-import java.util.Date
