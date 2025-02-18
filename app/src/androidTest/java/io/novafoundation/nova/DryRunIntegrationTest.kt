e0ca31a23348e6e31ffdb4732552f6bb24c0cfbd
valentunn <vallentunn@gmail.com>
2025-02-18 14:09:30 +0300

    Uncomment test

--- app/src/androidTest/java/io/novafoundation/nova/DryRunIntegrationTest.kt
+++ app/src/androidTest/java/io/novafoundation/nova/DryRunIntegrationTest.kt
@@ -3,7 +3,11 @@ package io.novafoundation.nova
 import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import io.novafoundation.nova.common.di.FeatureUtils
+import io.novafoundation.nova.common.utils.Modules
+import io.novafoundation.nova.common.utils.composeCall
 import io.novafoundation.nova.feature_xcm_api.di.XcmFeatureApi
+import io.novafoundation.nova.feature_xcm_api.dryRun.model.OriginCaller
+import io.novafoundation.nova.runtime.multiNetwork.getRuntime
 import org.junit.Test
 
 class DryRunIntegrationTest : BaseIntegrationTest() {
@@ -17,18 +21,18 @@ class DryRunIntegrationTest : BaseIntegrationTest() {
 
     @Test
     fun testDryRunCall() = runTest {
-//        val polkadot = chainRegistry.polkadot()
-//        val polkadotRuntime = chainRegistry.getRuntime(polkadot.id)
-//
-//        val result = dryRunApi.dryRunCall(
-//            originCaller = OriginCaller.System.Root,
-//            call = polkadotRuntime.composeCall(
-//                moduleName = Modules.SYSTEM,
-//                callName = "remark_with_event",
-//                args = emptyMap()
-//            ),
-//            chainId = polkadot.id
-//        )
-//        println(result)
+        val polkadot = chainRegistry.polkadot()
+        val polkadotRuntime = chainRegistry.getRuntime(polkadot.id)
+
+        val result = dryRunApi.dryRunCall(
+            originCaller = OriginCaller.System.Root,
+            call = polkadotRuntime.composeCall(
+                moduleName = Modules.SYSTEM,
+                callName = "remark_with_event",
+                args = emptyMap()
+            ),
+            chainId = polkadot.id
+        )
+        println(result)
     }
 }
