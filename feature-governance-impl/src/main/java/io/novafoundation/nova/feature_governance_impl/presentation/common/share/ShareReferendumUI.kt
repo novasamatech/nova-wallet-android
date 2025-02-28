db50380330406214e333d660da3f2315f74b5297
antonijzelinskij <antonijzelinskij@gmail.com>
2025-02-28 09:22:46 +0100

    Fixed pr notes

--- feature-governance-impl/src/main/java/io/novafoundation/nova/feature_governance_impl/presentation/common/share/ShareReferendumUI.kt
+++ feature-governance-impl/src/main/java/io/novafoundation/nova/feature_governance_impl/presentation/common/share/ShareReferendumUI.kt
@@ -1,15 +1,10 @@
 package io.novafoundation.nova.feature_governance_impl.presentation.common.share
 
-import android.content.Intent
 import io.novafoundation.nova.common.base.BaseFragment
+import io.novafoundation.nova.common.utils.shareText
 
 fun BaseFragment<*>.setupReferendumSharing(mixin: ShareReferendumMixin) {
     mixin.shareEvent.observeEvent { referendumLink ->
-        val intent = Intent(Intent.ACTION_SEND).apply {
-            type = "text/plain"
-            putExtra(Intent.EXTRA_TEXT, referendumLink.toString())
-        }
-
-        startActivity(Intent.createChooser(intent, null))
+        requireContext().shareText(referendumLink.toString())
     }
 }
