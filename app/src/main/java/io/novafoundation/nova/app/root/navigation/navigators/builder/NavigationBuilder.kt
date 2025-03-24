ba6953e4cc77ca3433b0191ac20b9045805ebe01
antonijzelinskij <antonijzelinskij@gmail.com>
2025-03-24 10:45:42 +0100

    Update navigation docs

--- app/src/main/java/io/novafoundation/nova/app/root/navigation/navigators/builder/NavigationBuilder.kt
+++ app/src/main/java/io/novafoundation/nova/app/root/navigation/navigators/builder/NavigationBuilder.kt
@@ -13,9 +13,18 @@ import io.novafoundation.nova.app.root.navigation.navigators.NavigationHoldersRe
  * - root-navigator - a navigator that opens fragments on top of others and is designed for fragments that do not require a split screen (For example, DAppBrowser needs to be opened in the root navigator)
  * - split_screen-navigator - the main navigator of the application. All fragments opened in it will be opened in a split screen mode
  *
- * When building navigation, it is usually sufficient to add a new node to split_screen_navigation_graph, but there are cases when this node needs to be added to both graphs at once
- * (For example, the transaction confirmation screen, we need to open both in the split screen and directly in the browser)
- * To handle such cases, we use the [navigateInFirstAttachedContext] method, which by default tries to open the fragment in split_screen, but if it fails, opens the fragment in root
+ * Let's look at the scenarios for building navigation:
+ * - In the normal case, you need to add a navigation node only to the split_screen_navigation_graph, be it a dialog or a fragment.
+ *   This can be used when you are sure that the fragment or dialog should not be launched in the browser or on top of a split screen.
+ *   Use [navigateInFirstAttachedContext] and the fragment will be automatically attached to the SplitScreenNavigationHolder.
+ * - If you expect that the fragment can also be launched from the browser, you need to add it to both the root_navigation_graph and the split_screen_navigation_graph.
+ *   Keep in mind that the actionId must be the same in both graphs.
+ *   Use [navigateInFirstAttachedContext] and the fragment will be automatically attached to the desired holder.
+ * - In case of adding dialogs, you can add it only to the root_navigation_graph if you think that the dialog can be launched both from the browser and from the remote part of the application.
+ *   To attach the dialog to the RootNavigationHolder, call [navigateInRoot]
+ * - In the latter case, we may need to add a screen that is strictly required to be opened on top of the split screen or only in the browser flow. (Such screens as entering a pin code).
+ *   In this case, we need to add an action only to the root_navigation_graph
+ *   To attach the the fragment to the RootNavigationHolder, call [navigateInRoot]
  **/
 abstract class NavigationBuilder (
     private val navigationHoldersRegistry: NavigationHoldersRegistry
