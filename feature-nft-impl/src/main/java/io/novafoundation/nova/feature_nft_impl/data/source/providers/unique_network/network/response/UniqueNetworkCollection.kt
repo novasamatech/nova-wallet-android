a9c676abd3dc7d4b8ff6393ccf319c5ac95747ba
ashkuc <ashkucc@gmail.com>
2025-06-02 19:15:37 +0200

    remove unused fields from NFT and Collection responses

--- feature-nft-impl/src/main/java/io/novafoundation/nova/feature_nft_impl/data/source/providers/unique_network/network/response/UniqueNetworkCollection.kt
+++ feature-nft-impl/src/main/java/io/novafoundation/nova/feature_nft_impl/data/source/providers/unique_network/network/response/UniqueNetworkCollection.kt
@@ -3,20 +3,12 @@ package io.novafoundation.nova.feature_nft_impl.data.source.providers.unique_net
 data class UniqueNetworkCollection(
     val collectionId: Int,
     val name: String?,
-    val description: String?,
     val coverImage: CoverImage?,
-    val owner: String,
-    val propertiesMap: Map<String, Property>?,
     val lastTokenId: Int?,
     val limits: Limits?
 ) {
     data class CoverImage(
         val url: String?
-    )
-
-    data class Property(
-        val key: String,
-        val value: String
     )
 
     data class Limits(
--- feature-nft-impl/src/main/java/io/novafoundation/nova/feature_nft_impl/data/source/providers/unique_network/network/response/UniqueNetworkNft.kt
+++ feature-nft-impl/src/main/java/io/novafoundation/nova/feature_nft_impl/data/source/providers/unique_network/network/response/UniqueNetworkNft.kt
@@ -4,23 +4,6 @@ data class UniqueNetworkNft(
     val key: String,
     val collectionId: Int,
     val tokenId: Int,
-    val owner: String,
-    val topmostOwner: String,
-    val isBundle: Boolean,
     val image: String?,
     val name: String?,
-    val description: String?,
-    val attributes: List<Attribute>?,
-    val propertiesMap: Map<String, Property>?,
-    val isBurned: Boolean
-) {
-    data class Attribute(
-        val trait_type: String,
-        val value: String
-    )
-
-    data class Property(
-        val key: String,
-        val value: String
-    )
-}
+)
