033f3025c78ce193182d34d07f5d407cbf7e91d4
antonijzelinskij <antonijzelinskij@gmail.com>
2025-06-24 12:25:27 +0200

    Update MS entry point

--- feature-assets/src/main/java/io/novafoundation/nova/feature_assets/presentation/balance/list/view/PendingOperationsCountView.kt
+++ feature-assets/src/main/java/io/novafoundation/nova/feature_assets/presentation/balance/list/view/PendingOperationsCountView.kt
@@ -3,12 +3,9 @@ package io.novafoundation.nova.feature_assets.presentation.balance.list.view
 import android.content.Context
 import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
-import io.novafoundation.nova.common.utils.addRipple
-import io.novafoundation.nova.common.utils.getRoundedCornerDrawable
 import io.novafoundation.nova.common.utils.inflater
 import io.novafoundation.nova.common.utils.makeGone
 import io.novafoundation.nova.common.utils.makeVisible
-import io.novafoundation.nova.feature_assets.R
 import io.novafoundation.nova.feature_assets.databinding.ViewPendingOperationsCountBinding
 
 class PendingOperationsCountView @JvmOverloads constructor(
@@ -18,10 +15,6 @@ class PendingOperationsCountView @JvmOverloads constructor(
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
     private val binding = ViewPendingOperationsCountBinding.inflate(inflater(), this)
-
-    init {
-        background = addRipple(getRoundedCornerDrawable(R.color.block_background))
-    }
 
     fun setPendingOperationsCount(model: PendingOperationsCountModel) {
         when (model) {
