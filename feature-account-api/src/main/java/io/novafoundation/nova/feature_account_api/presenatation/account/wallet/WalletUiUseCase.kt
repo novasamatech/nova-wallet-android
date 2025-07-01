b62fe01ded40283c1792cbc8717c54f0326f1dd7
antonijzelinskij <antonijzelinskij@gmail.com>
2025-07-01 11:08:55 +0200

    Fixed crash on ms operation for eth-based chain

--- feature-account-api/src/main/java/io/novafoundation/nova/feature_account_api/presenatation/account/wallet/WalletUiUseCase.kt
+++ feature-account-api/src/main/java/io/novafoundation/nova/feature_account_api/presenatation/account/wallet/WalletUiUseCase.kt
@@ -2,7 +2,9 @@ package io.novafoundation.nova.feature_account_api.presenatation.account.wallet
 
 import android.graphics.drawable.Drawable
 import io.novafoundation.nova.common.address.AddressIconGenerator
+import io.novafoundation.nova.common.address.AddressModel
 import io.novafoundation.nova.feature_account_api.domain.model.MetaAccount
+import io.novafoundation.nova.runtime.multiNetwork.chain.model.Chain
 import io.novasama.substrate_sdk_android.runtime.AccountId
 import kotlinx.coroutines.flow.Flow
 
@@ -34,6 +36,8 @@ interface WalletUiUseCase {
     suspend fun walletIcon(metaAccount: MetaAccount, iconSize: Int = AddressIconGenerator.SIZE_MEDIUM, transparentBackground: Boolean = true): Drawable
 
     suspend fun walletUiFor(metaAccount: MetaAccount): WalletModel
+
+    suspend fun walletAddressModel(metaAccount: MetaAccount, chain: Chain, iconSize: Int): AddressModel
 }
 
 fun WalletUiUseCase.walletUiFlowFor(metaId: Long, chainId: String?, showAddressIcon: Boolean = false): Flow<WalletModel> {
