b4759a4ac824d3269f639d466076ac47be188872
antonijzelinskij <antonijzelinskij@gmail.com>
2025-07-11 13:40:42 +0200

    Update RootScope.kt

--- common/src/main/java/io/novafoundation/nova/common/utils/coroutines/RootScope.kt
+++ common/src/main/java/io/novafoundation/nova/common/utils/coroutines/RootScope.kt
@@ -3,4 +3,5 @@ package io.novafoundation.nova.common.utils.coroutines
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
 
+@Deprecated("Use scope in RootViewModel instead")
 class RootScope : CoroutineScope by MainScope()
