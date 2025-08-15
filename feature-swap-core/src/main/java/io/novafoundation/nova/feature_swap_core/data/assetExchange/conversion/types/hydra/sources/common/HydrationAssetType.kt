d59102c06b020fa2465c28821347aa7e47924b30
valentunn <70131744+valentunn@users.noreply.github.com>
2025-08-15 18:09:54 +0300

    Feature/gdot (#2080)
    * Ad GDOT support
    
    * Fix swaps - balance loading for erc20 tokens and support pegs in stableswap math
    
    * Remove test code
    
    * Fix GDOT transfers
    
    * Fix GDOT transfers
    
    * Make ktlint happy by not using newer kotlin features yet
    
    * Fix PR reviews

---
+++ feature-swap-core/src/main/java/io/novafoundation/nova/feature_swap_core/data/assetExchange/conversion/types/hydra/sources/common/HydrationAssetType.kt
@@ -0,0 +1,28 @@
+package io.novafoundation.nova.feature_swap_core.data.assetExchange.conversion.types.hydra.sources.common
+
+import io.novafoundation.nova.feature_swap_core_api.data.network.HydraDxAssetId
+import io.novafoundation.nova.runtime.multiNetwork.chain.model.Chain
+import io.novafoundation.nova.runtime.multiNetwork.chain.model.Chain.Asset.Type.Orml.SubType
+
+sealed class HydrationAssetType {
+
+    companion object;
+
+    data object Native : HydrationAssetType()
+
+    class Orml(val assetId: HydraDxAssetId) : HydrationAssetType()
+
+    class Erc20(val assetId: HydraDxAssetId) : HydrationAssetType()
+}
+
+fun HydrationAssetType.Companion.fromAsset(chainAsset: Chain.Asset, hydrationAssetId: HydraDxAssetId): HydrationAssetType {
+    return when (val type = chainAsset.type) {
+        is Chain.Asset.Type.Native -> HydrationAssetType.Native
+        is Chain.Asset.Type.Orml -> when (type.subType) {
+            SubType.DEFAULT -> HydrationAssetType.Orml(hydrationAssetId)
+            SubType.HYDRATION_EVM -> HydrationAssetType.Erc20(hydrationAssetId)
+        }
+
+        else -> throw IllegalArgumentException("Unsupported asset type: ${chainAsset.type}")
+    }
+}
