7ba30d4ad0e94dfc7a672c489322af7615891d68
antonijzelinskij <107959809+antonijzelinskij@users.noreply.github.com>
2026-08-12 19:36:57 +0400

    Feature/consent screen (#2307)
    * ToS implementation
    
    * Improved date mapping on remote
    
    * Update NewSubstrateLedgerApplication.kt
    
    * Added common consent view for all ToS cases
    
    * Update strings.xml
    
    * Update strings.xml
    
    * Update WelcomeViewModel.kt

--- common/src/main/java/io/novafoundation/nova/common/utils/formatting/TermsAndPrivacyFormatting.kt
+++ common/src/main/java/io/novafoundation/nova/common/utils/formatting/TermsAndPrivacyFormatting.kt
@@ -7,9 +7,11 @@ import android.widget.TextView
 import io.novafoundation.nova.common.R
 import io.novafoundation.nova.common.utils.clickableSpan
 import io.novafoundation.nova.common.utils.colorSpan
+import io.novafoundation.nova.common.utils.dp
 import io.novafoundation.nova.common.utils.formatting.spannable.SpannableFormatter
 import io.novafoundation.nova.common.utils.setFullSpan
 import io.novafoundation.nova.common.utils.toSpannable
+import io.novafoundation.nova.common.utils.updatePadding
 
 fun TextView.applyTermsAndPrivacyPolicy(
     containerResId: Int,
@@ -19,6 +21,7 @@ fun TextView.applyTermsAndPrivacyPolicy(
     privacyClicked: () -> Unit,
     underlineLinks: Boolean = false
 ) {
+    updatePadding(bottom = 4.dp) // Add padding bottom to let link be underlined
     movementMethod = LinkMovementMethod.getInstance()
     highlightColor = Color.TRANSPARENT
     val linkColor = context.getColor(R.color.text_primary)
--- feature-onboarding-impl/src/main/java/io/novafoundation/nova/feature_onboarding_impl/presentation/welcome/WelcomeViewModel.kt
+++ feature-onboarding-impl/src/main/java/io/novafoundation/nova/feature_onboarding_impl/presentation/welcome/WelcomeViewModel.kt
@@ -7,9 +7,13 @@ import io.novafoundation.nova.common.data.legal.LegalConsentRepository
 import io.novafoundation.nova.common.data.network.AppLinksProvider
 import io.novafoundation.nova.common.mixin.api.Browserable
 import io.novafoundation.nova.common.utils.Event
+import io.novafoundation.nova.common.utils.shareInBackground
+import io.novafoundation.nova.feature_account_api.domain.interfaces.AccountRepository
 import io.novafoundation.nova.feature_account_api.presenatation.account.add.AddAccountPayload
 import io.novafoundation.nova.feature_onboarding_impl.OnboardingRouter
 import io.novafoundation.nova.feature_versions_api.domain.UpdateNotificationsInteractor
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class WelcomeViewModel(
     shouldShowBack: Boolean,
@@ -17,6 +21,7 @@ class WelcomeViewModel(
     private val appLinksProvider: AppLinksProvider,
     private val addAccountPayload: AddAccountPayload,
     private val legalConsentRepository: LegalConsentRepository,
+    private val accountRepository: AccountRepository,
     updateNotificationsInteractor: UpdateNotificationsInteractor
 ) : BaseViewModel(),
     Browserable {
@@ -25,8 +30,16 @@ class WelcomeViewModel(
 
     override val openBrowserEvent = MutableLiveData<Event<String>>()
 
+    private val consentAccepted = MutableStateFlow(false)
+
+    val canProceed: Flow<Boolean> = consentAccepted.shareInBackground()
+
     init {
         updateNotificationsInteractor.allowInAppUpdateCheck()
+    }
+
+    fun consentCheckChanged(checked: Boolean) {
+        consentAccepted.value = checked
     }
 
     fun createAccountClicked() {
@@ -57,8 +70,8 @@ class WelcomeViewModel(
     }
 
     /**
-     * Implicit consent: proceeding from this screen counts as accepting the current documents,
-     * as stated by the disclaimer under the buttons
+     * Proceeding from this screen counts as accepting the current documents - either explicitly via the
+     * checkbox, or implicitly as stated by the disclaimer under the buttons
      */
     private fun acceptLegalDocuments() {
         legalConsentRepository.acceptCurrentVersions()
